fa0626cc5ce221f8a09004ce1fc3757bc12ed6b9
Daniel Heath <daniel@heath.cc>
2015-07-10 09:19:14 +1000

    Fixup webpack config: bundle.js lives in the top level of the site ('dist'), not in /dist/bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     entry: "./src/entry.js",
     output: {
-        path: __dirname,
-        filename: "dist/bundle.js"
+        path: __dirname + "/dist",
+        filename: "bundle.js"
     },
     module: {
         loaders: [
