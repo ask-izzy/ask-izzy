c6bdaf87ef1f167f338cf5206a23784d53c0c7cb
Danielle Madeley <dmadeley@infoxchange.net.au>
2015-07-13 17:32:36 +1000

    Enable Selenium via Sauce Labs

--- test/yadda.js
+++ test/yadda.js
@@ -4,6 +4,7 @@
 "use strict";
 
 const Yadda = require('yadda');
+const Webdriver = require('selenium-webdriver');
 
 Yadda.plugins.mocha.StepLevelPlugin.init();
 
@@ -11,12 +12,62 @@ const library = require('./steps/steps');
 
 new Yadda.FeatureFileSearch('./test/features').each(file => {
     featureFile(file, feature => {
-        let yadda = Yadda.createInstance(library);
+        let driver;
+        let yadda = Yadda.createInstance(library, {
+            drivers: driver
+        });
+
+        before(done => {
+            executeInFlow(() => {
+
+                let builder = new Webdriver.Builder()
+                    .forBrowser('firefox');
+
+                if (process.env.SAUCE_USERNAME) {
+                    builder = builder.usingServer(
+                        'http://' +
+                        process.env.SAUCE_USERNAME +
+                        process.env.SAUCE_ACCESS_KEY +
+                        '@ondemand.saucelabs.com:80/wd/hub')
+                }
+
+                driver = builder.build()
+                driver.manage().timeouts().implicitlyWait(10000);
+            }, done);
+        });
 
         scenarios(feature.scenarios, scenario => {
-            steps(scenario.steps, step => {
-                yadda.run(step);
+            steps(scenario.steps, (step, done) => {
+                executeInFlow(() => {
+                    yadda.run(step);
+                }, done);
             });
+        });
+
+        /* IMPORTANT: needs the correct 'this' */
+        afterEach(function() {
+            takeScreenshotOnFailure(this.currentTest);
+        });
+
+        after(done => {
+            driver.quit().then(done);
         });
     });
 });
+
+function executeInFlow(fn, done) {
+    Webdriver.promise.controlFlow().execute(fn).then(function() {
+        done();
+    }, done);
+}
+
+function takeScreenshotOnFailure(test) {
+    if (test.state != 'passed') {
+        var path = 'screenshots/' +
+            test.title.replace(/\W+/g, '_').toLowerCase() + '.png';
+
+        driver.takeScreenshot().then(function(data) {
+            fs.writeFileSync(path, data, 'base64');
+        });
+    }
+}
