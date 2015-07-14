bf41cef47b6152d0e9bda58889059b9ea637647e
Daniel Heath <daniel@heath.cc>
2015-07-14 16:57:56 +1000

    Follow airbnb style

--- test/steps/steps.js
+++ test/steps/steps.js
@@ -7,12 +7,12 @@
 import Yadda from "yadda";
 
 module.exports = (function() {
-    return Yadda.localisation.English.library().
-        when('I visit $URL', url => {
+    return Yadda.localisation.English.library()
+        .when('I visit $URL', url => {
             console.log("Visiting", url);
-        }).
+        })
 
-        then('I should see "$STRING"', expected => {
+        .then('I should see "$STRING"', expected => {
             console.log("Do I see", expected);
         });
 })();
