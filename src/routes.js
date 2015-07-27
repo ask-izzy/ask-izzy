16978a805759e78c9b0710211bf0b6880435cc4d
Daniel Heath <daniel@heath.cc>
2015-07-27 11:24:16 +1000

    Add built-in styleguide

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,8 @@ import BasePage from "./pages/BasePage";
 import HomePage from "./pages/HomePage";
 import ErrorPage from "./pages/ErrorPage";
 import CategoryPage from "./pages/CategoryPage";
+import StyleGuideList from "./pages/StyleGuideList";
+import StyleGuideItem from "./pages/StyleGuideItem";
 
 export default <Router.Route
         name="root"
@@ -17,6 +19,16 @@ export default <Router.Route
             name="home"
             path="/"
             handler={HomePage}
+        />
+        <Router.Route
+            name="styleguideItem"
+            path="/styleGuide/component/:componentName"
+            handler={StyleGuideItem}
+        />
+        <Router.Route
+            name="styleguideList"
+            path="/styleGuide*"
+            handler={StyleGuideList}
         />
         <Router.Route
             name="category"
