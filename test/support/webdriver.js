aed6eabced907efc6f736aed24b67b2437ba7fda
Daniel Heath <daniel@heath.cc>
2015-08-04 16:53:45 +1000

    Add the browsers Mark recommended for automated testing.

--- test/support/webdriver.js
+++ test/support/webdriver.js
@@ -28,21 +28,28 @@ export function executeInFlow(fn, done) {
 export default function webDriverInstance() {
     let branch = process.env.TRAVIS_BRANCH || "Manual";
 
+    let baseCaps = {
+        username: process.env.SAUCE_USERNAME,
+        accessKey: process.env.SAUCE_ACCESS_KEY,
+        name: "Ask Izzy " + branch,
+        tags: [
+            process.env.TRAVIS_PULL_REQUEST || "Manual",
+            branch,
+        ],
+        build: process.env.TRAVIS_BUILD_NUMBER || "Manual",
+        tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
+    }
+    if (process.env.SELENIUM_DEVICE) {
+        baseCaps.deviceName = process.env.SELENIUM_DEVICE
+    }
+    if (process.env.SELENIUM_ORIENTATION) {
+        baseCaps.deviceOrientation = process.env.SELENIUM_ORIENTATION
+    }
     var driver = new Webdriver.Builder()
         /* These are used by Sauce Labs
          * You should also pass SELENIUM_REMOTE_URL to connect
          * via Selenium Grid */
-        .withCapabilities({
-            username: process.env.SAUCE_USERNAME,
-            accessKey: process.env.SAUCE_ACCESS_KEY,
-            name: "Ask Izzy " + branch,
-            tags: [
-                process.env.TRAVIS_PULL_REQUEST || "Manual",
-                branch,
-            ],
-            build: process.env.TRAVIS_BUILD_NUMBER || "Manual",
-            tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
-        })
+        .withCapabilities(baseCaps)
         /* This is the default. Overridden by SELENIUM_BROWSER */
         .forBrowser('firefox')
         .build();
