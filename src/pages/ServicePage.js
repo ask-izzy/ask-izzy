71ee640675feaa2fb105efb20994b0cd13e34a85
Danielle Madeley <dmadeley@infoxchange.net.au>
2015-08-18 16:37:45 +1000

    Add a dummy service page

---
+++ src/pages/ServicePage.js
@@ -0,0 +1,58 @@
+/* @flow */
+
+import mui from "material-ui";
+import React from 'react';
+import Router from 'react-router';
+
+import iss from '../iss';
+
+class CategoryPage extends React.Component {
+    constructor(props: Object) {
+        super(props);
+        this.state = {
+            object: {
+                site: {},
+            },
+        };
+    }
+
+    /**
+     * search:
+     * Do a search in ISS
+     *
+     * Returns: a promise to an object of data from ISS.
+     */
+
+    componentDidMount(): void {
+        iss(`service/${this.props.params.id}/`)
+            .then(data => {
+                console.log(data);
+                this.setState({
+                    object: data,
+                });
+            });
+
+        // FIXME: display error
+    }
+
+    render(): React.Element {
+        var {
+            object,
+        } = this.state;
+
+        return (
+            <div>
+                <Router.RouteHandler />
+                <mui.AppBar title={object.name} />
+                <mui.Paper>
+                    <p>
+                        {object.site.name}
+                    </p>
+                </mui.Paper>
+            </div>
+        );
+    }
+
+}
+
+export default CategoryPage;
