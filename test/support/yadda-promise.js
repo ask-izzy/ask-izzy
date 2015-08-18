a1747ae6e2b860b4a0593cc3141c681eeccb17d6
Daniel Heath <daniel@heath.cc>
2015-08-18 13:01:10 +1000

    Explain what this does

--- test/support/yadda-promise.js
+++ test/support/yadda-promise.js
@@ -1,5 +1,8 @@
 export default function unpromisify(fn) {
     return function promisified(...args) {
+        // The last argument is a callback(error, result)
+        // but there's no success value, so we just strip
+        // all arguments on success.
         var done = args.splice(args.length - 1, 1)[0];
         try {
             fn.apply(this, args).then(stripArgs(done)).catch(done);
