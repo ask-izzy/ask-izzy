e4458e375c0487a5d17c4b7e276a6932070a55e1
Danielle Madeley <dmadeley@infoxchange.org>
2015-08-21 19:22:01 +1000

    Support touch events

--- src/client.js
+++ src/client.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Router from "react-router";
+import injectTapEventPlugin from "react-tap-event-plugin";
+
 import routes from "./routes";
 
 // Import CSS
@@ -7,6 +9,9 @@ import routes from "./routes";
 import "!style!css!./styles/normalize.css";
 import "!style!css!sass!./styles/responsive.scss";
 import "!style!css!sass!./styles/components.scss";
+
+// For onTouchTap: see https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 window.debug = require("debug");
 const debug = window.debug("ask-izzy");
@@ -24,5 +29,4 @@ Router.run(
 
         );
     },
-
 );
