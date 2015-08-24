49455930aed26dc650d83c4c1b093cc6c46e9f32
Danielle Madeley <dmadeley@infoxchange.org>
2015-08-24 13:28:03 +1000

    Add flow hints to unpromisify

--- test/support/yadda-promise.js
+++ test/support/yadda-promise.js
@@ -1,18 +1,25 @@
-export default function unpromisify(fn) {
+/* @flow */
+
+"use strict";
+
+function unpromisify(fn: (...args: any) => Promise<void>): Function {
+    function stripArgs(fn) {
+        return () => fn();
+    }
+
     return function promisified(...args) {
         // The last argument is a callback(error, result)
         // but there's no success value, so we just strip
         // all arguments on success.
         var done = args.splice(args.length - 1, 1)[0];
         try {
-            fn.apply(this, args).then(stripArgs(done)).catch(done);
+            fn.apply(this, args)
+                .then(stripArgs(done))
+                .catch(done);
         } catch (e) {
             done(e);
         }
     };
-
-    function stripArgs(fn) {
-        return () => fn();
-    }
-
 }
+
+export default unpromisify;
