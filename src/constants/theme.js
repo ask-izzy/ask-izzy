fa665fcb490ce92edbba3002ec738830c3238c88
Danielle Madeley <dmadeley@infoxchange.org>
2015-08-25 12:22:53 +1000

    Fix text colours

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,15 +1,17 @@
 // Override colors & themes here
 export default {
+    contentFontFamily: 'Gotham Rounded A,Gotham Rounded B,sans-serif',
+
     getPalette: function() {
         return {
             accent1Color: "#EEEEEE",
             accent2Color: "#999999",
-            secondaryTextColor: "#666666",
+            textColor: "#363e43",  // brand-text-dark
+            secondaryTextColor: "#767676",  // brand-text-mid
         };
     },
 
     getComponentThemes(palette) {
         return {};
     },
-
 };
