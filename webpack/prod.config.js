02fbe44fc1e1b8abe8d9ef156b404da7fb86b5b8
Daniel Heath <daniel@heath.cc>
2015-08-26 16:32:06 +1000

    Automatically apply vendor prefixes to css rules.
    Also, fix FOUC on page load by extracting separate CSS files.

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -7,13 +7,17 @@ import webpack           from "webpack";
 import writeStats        from "./utils/write-stats";
 import strip             from "strip-loader";
 import env               from "./env";
+import extractText       from "./extract-text";
 
 var assetsPath = path.join(__dirname, "../public/static");
 
 module.exports = {
     devtool: "source-map",
     entry: {
-        main: "./src/client-entry.js",
+        main: [
+            "./src/client-entry.js",
+            "./src/styles/bundle.scss",
+        ],
     },
     output: {
         path: assetsPath,
@@ -28,12 +32,9 @@ module.exports = {
                 loader: "file",
             },
             {
-                test: /\.css$/,
-                loader: "style!css",
-            },
-            {
                 test: /\.scss$/,
-                loader: "style!css!sass",
+                exclude: /node_modules/,
+                loader: extractText.loaders,
             },
             {
                 test: /\.js$/,
@@ -55,6 +56,8 @@ module.exports = {
         new webpack.ProvidePlugin({
             'es6-promise': 'es6-promise',
         }),
+
+        extractText.plugin,
 
         // optimizations
         new webpack.optimize.DedupePlugin(),
