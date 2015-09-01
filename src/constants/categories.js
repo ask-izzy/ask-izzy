85647e83c31e5849af33d66b4f7d08a08c90c021
Danielle Madeley <dmadeley@infoxchange.org>
2015-09-01 11:29:41 +1000

    Search on housing service type

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -35,7 +35,10 @@ var categories:Array<Category> = [
         name: "Housing",
         byline: "Find a place to stay",
         icon: icons.House,
-        search: 'housing',
+        search: {
+            q: 'housing',
+            service_type: 'housing',
+        },
         info: (
             <Infobox href="#" linkText="Housing information">
                 It's important to act early on housing. These services can
@@ -48,79 +51,105 @@ var categories:Array<Category> = [
         name: "Food",
         byline: "Find something to eat",
         icon: icons.Food,
-        search: 'food',
+        search: {
+            q: 'food',
+        },
     }),
     new Category({
         name: "Everyday things",
         byline: "Things you might need",
         icon: icons.Things,
-        search: 'recreation',
+        search: {
+            q: 'recreation',
+        },
     }),
     new Category({
         name: "Legal",
         byline: "Help with law, police & transport officers",
         icon: icons.Legal,
-        search: 'legal aid',
+        search: {
+            q: 'legal aid',
+        },
     }),
     new Category({
         name: "Money help",
         byline: "Emergency funds, bills or budgeting",
         icon: icons.Money,
-        search: 'financial aid',
+        search: {
+            q: 'financial aid',
+        },
     }),
     new Category({
         name: "Health",
         byline: "Physical, mental & emotional health",
         icon: icons.Health,
-        search: 'physical health',
+        search: {
+            q: 'physical health',
+        },
     }),
     new Category({
         name: "Addiction",
         byline: "Dealing with drugs & alcohol, gambling etc.",
         icon: icons.Addiction,
-        search: 'additional',
+        search: {
+            q: 'addiction',
+        },
     }),
     new Category({
         name: "Support & counselling",
         byline: "Someone to help & talk to",
         icon: icons.Mental,
-        search: 'mental health',
+        search: {
+            q: 'mental health',
+        },
     }),
     new Category({
         name: "Life skills & education",
         byline: "Developing everyday living skills etc.",
         icon: icons.Skills,
-        search: 'life skills',
+        search: {
+            q: 'life skills',
+        },
     }),
     new Category({
         name: "Finding work",
         byline: "Making money or volunteering",
         icon: icons.Job,
-        search: 'employment',
+        search: {
+            q: 'employment',
+        },
     }),
     new Category({
         name: "Centrelink",
         byline: "Find Centrelink near you",
         icon: icons.Centrelink,
-        search: 'centrelink',
+        search: {
+            q: 'centrelink',
+        },
     }),
     new Category({
         name: "Something to do",
         byline: "Meet people, try an activity or just hang out",
         icon: icons.Entertainment,
-        search: 'recreation',
+        search: {
+            q: 'recreation',
+        },
     }),
     new Category({
         name: "Facilities",
         byline: "Public toilets, libraries, etc.",
         icon: icons.Facilaties,
-        search: 'facilities',
+        search: {
+            q: 'libraries toilets',
+        },
     }),
     new Category({
         name: "Technology",
         byline: "Computers, mobiles, free wifi or charging",
         icon: icons.Tech,
-        search: 'technology',
+        search: {
+            q: 'computers wifi',
+        },
     }),
 ];
 
