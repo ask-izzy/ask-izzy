36d5df87bd00e6a17a1d1b598bf14d73968b7547
Daniel Heath <daniel@heath.cc>
2015-09-15 17:00:02 +1000

    Set timezone offset

--- test/steps/datetime.js
+++ test/steps/datetime.js
@@ -24,13 +24,15 @@ async function changeDay(day: string): Promise<void> {
         .startOf('day')
         .add(18, 'h')
         .valueOf();
+    var offset = new Date().getTimezoneOffset();
     var script = fs.readFileSync(`${__dirname}/../support/timeshift.js`,
                                  {encoding: 'utf-8'});
 
     await this.driver.executeScript(script);
-    await this.driver.executeScript(time => {
+    await this.driver.executeScript((time, offset) => {
         Date = TimeShift.Date;
+        TimeShift.setTimezoneOffset(offset);
         TimeShift.setTime(time);
         console.log("Mocked time to", new Date());
-    }, time);
+    }, time, offset);
 }
