6316f87adc70330f8df13f3d8f9a83bd4898456d
Daniel Heath <daniel@heath.cc>
2015-10-15 14:27:55 +1100

    Explain use of GLOBAL

--- src/server.js
+++ src/server.js
@@ -10,11 +10,15 @@ if (!process.env.ISS_URL) {
     console.error("Must set ISS_URL");
     process.exit(1);
 }
-GLOBAL.ISS_URL = process.env.ISS_URL;
 
 if (!process.env.GOOGLE_KEY) {
     console.warn("GOOGLE_KEY not set");
 }
+
+// On the client, we set these variables on `window`
+// Making them globals on the server means the same
+// code can be used.
+GLOBAL.ISS_URL = process.env.ISS_URL;
 GLOBAL.GOOGLE_KEY = process.env.GOOGLE_KEY;
 
 // Initialize express server
