483bef488c3ed32e52cb895346218b80f402ab38
Danielle Madeley <dmadeley@infoxchange.org>
2015-10-30 15:18:53 +1100

    Add demographic keywords

--- src/pages/personalisation/Demographics.js
+++ src/pages/personalisation/Demographics.js
@@ -8,13 +8,14 @@ export default class Demographics extends BaseMultiQuestion {
         name: "demographics",
         question: "Do any of these apply to you?",
         answers: {
+            /* eslint-disable max-len */
             "Aboriginal or Torres Strait Islander": append("indigenous"),
-            "LGBTIQ": append(""), // "lgbt" doesn't work in ISS
+            "LGBTIQ": append("sexuality"),
             "Asylum seeker": append("refugee"),
             "Have a disability": append("disability"),
             "Have children": append("family"),
             "Have pets": append("pets"),
-            "On parole or recently released from prison": append("parole"),
+            "On parole or recently released from prison": append("post-release"),
             "No": append(""),
         },
     };
