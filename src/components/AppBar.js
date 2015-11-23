cd4d48e3bbef695fea4c9044f55764bbaa1cfab2
Daniel Heath <daniel@heath.cc>
2015-11-23 11:40:22 +1100

    AppBar should always have a home icon if no other icon is specified.

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -2,10 +2,13 @@
 
 import React from "react";
 import classnames from "classnames";
+import { History } from "react-router";
+import reactMixin from "react-mixin";
 
 import components from "../components";
 import icons from "../icons";
 
+/*::`*/@reactMixin.decorate(History)/*::`;*/
 class AppBar extends React.Component {
     static propTypes = {
         title: React.PropTypes.string.isRequired,
@@ -19,13 +22,25 @@ class AppBar extends React.Component {
     static defaultProps = {
         forwardEnabled: true,
         forwardMessage: "",
-        forwardIcon: <icons.Chevron />,
+        forwardIcon: <icons.House className="small" />,
     };
 
     static sampleProps = {default: {
         title: "App bar",
         onBackTouchTap: function() {},
     }};
+
+    onForwardTouchTap(): void {
+        if (this.props.onForwardTouchTap) {
+            this.props.onForwardTouchTap();
+        } else {
+            this.history.pushState(
+                null,
+                "/",
+                {}
+            );
+        }
+    }
 
     render(): ReactElement {
         return (
@@ -43,33 +58,24 @@ class AppBar extends React.Component {
     }
 
     renderForwardButton(): ?ReactElement {
-        if (this.props.onForwardTouchTap) {
-            return (
-                <components.IconButton
-                    className={classnames(
-                        "NextButton",
-                        "button-container",
-                        {slideIn: this.props.slideForwardIn},
-                    )}
-                    onTouchTap={this.props.onForwardTouchTap}
-                    disabled={!this.props.forwardEnabled}
-                >
-                    <div className="right">
-                        <span className="next-label">
-                            {this.props.forwardMessage}
-                        </span>
-                        {this.props.forwardIcon}
-                    </div>
-                </components.IconButton>
-            )
-        } else {
-            return (
-                <div className="button-container">
-                    <div className="right">
-                    </div>
+        return (
+            <components.IconButton
+                className={classnames(
+                    "NextButton",
+                    "button-container",
+                    {slideIn: this.props.slideForwardIn},
+                )}
+                onTouchTap={this.onForwardTouchTap.bind(this)}
+                disabled={!this.props.forwardEnabled}
+            >
+                <div className="right">
+                    <span className="next-label">
+                        {this.props.forwardMessage}
+                    </span>
+                    {this.props.forwardIcon}
                 </div>
-            )
-        }
+            </components.IconButton>
+        )
     }
 }
 
