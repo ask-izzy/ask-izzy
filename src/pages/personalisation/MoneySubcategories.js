2809dab2d2189e67393ac36210231cc905a45d0f
Daniel Heath <daniel@heath.cc>
2015-12-10 15:17:12 +1100

    Search for 'Financial counselling' in the name field and the main search

--- src/pages/personalisation/MoneySubcategories.js
+++ src/pages/personalisation/MoneySubcategories.js
@@ -16,7 +16,7 @@ export default class MoneySubcategories extends BaseMultiQuestion {
             "Financial assistance e.g. utility bills": "",
             "No interest loans": append("nils"),
             "Gambling counselling": remove("financial aid").append("gambling counselling"),
-            "Financial counselling": remove("aid").append("name:financial counselling"),
+            "Financial counselling": remove("financial aid").append('financial counselling name:"financial counselling"'),
         },
     };
 }
