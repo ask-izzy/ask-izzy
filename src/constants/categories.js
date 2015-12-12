592919727040604b42fca6fa92b7c55f34b04885
robynk <robyn@infoxchange.net.au>
2015-12-12 16:06:25 +1100

    Added keywords to facilities category

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -182,7 +182,7 @@ const categories:Array<Category> = [
         byline: "Toilets, libraries etc",
         icon: icons.Facilaties,
         search: {
-            q: "toilets public libraries -(coordinating bodies)",
+            q: "toilets public libraries pool -(coordinating bodies) -(hire of facilities)",
         },
         personalisation: [
             personalisation.Location,
