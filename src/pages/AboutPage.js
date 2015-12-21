abda3c101457e85a398a27c0460f3faf4b24fcf6
Daniel Heath <daniel@heath.cc>
2015-12-21 16:24:26 +1100

    Add about page copy

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -16,8 +16,35 @@ class AboutPage extends React.Component {
                 />
 
                 <div className="body">
-                    Ask Izzy is a guide to homeless
-                    support & prevention services.
+                    <p>
+                        Ask Izzy helps people who are homeless
+                        or at risk of becoming
+                        homeless to find the services they need,
+                        right now and nearby.
+                    </p>
+
+                    <h3>Ask Izzy:</h3>
+                    <ul>
+                        <li>
+                            is nationwide and free.
+                        </li>
+                        <li>
+                            can help you find housing, meals,
+                            health services, counselling, legal help,
+                            addiction help and a whole lot more services.
+                        </li>
+                        <li>
+                            lists more than 340 000 different services.
+                        </li>
+                        <li>
+                            is available on phones, tablets and computers.
+                        </li>
+                    </ul>
+                    <p>
+                        We acknowledge the traditional custodians of this
+                        land and pay our respects to their culture, their
+                        people and elders past and present.
+                    </p>
                 </div>
             </div>
         );
