e7c3f8e1c2f6b356f1bd5a589effd02bf989c2c2
Daniel Heath <daniel@heath.cc>
2015-12-21 14:00:37 +1100

    Fixup long line

--- src/pages/personalisation/LegalSubcategories.js
+++ src/pages/personalisation/LegalSubcategories.js
@@ -16,7 +16,8 @@ export default class LegalSubcategories extends BaseMultiQuestion {
             "Tenancy issues": append("tenancy"),
             "Victims of crime compensation": remove("law")
                 .append("victims of crime -police"),
-            "Criminal charges": append("criminal charge criminal law -police"),
+            "Criminal charges":
+                append("criminal charge criminal law -police"),
         },
     };
 }
