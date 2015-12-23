1ff1d4da3db924505cb27f52fc2f8ae7fc33b0ba
Daniel Heath <daniel@heath.cc>
2015-12-23 15:57:44 +1100

    Merge two PRs into one and get the tests working with both

--- test/unit/search-composing.js
+++ test/unit/search-composing.js
@@ -20,7 +20,7 @@ describe("Compose personalisation search terms", function() {
         // flow:disable -- flow is confused about exports
         request = pages.Gender.getSearchForAnswer(request, "Female");
         assert.deepEqual(request, {
-            q: "crisis accommodation",
+            q: "crisis accommodation females",
             client_gender: ["f", "u"],
         });
     });
@@ -52,7 +52,7 @@ describe("Compose personalisation search terms", function() {
         // flow:disable -- flow is confused about exports
         request = pages.Age.getSearchForAnswer(request, "26 to 39");
         assert.deepEqual(request, {
-            q: "crisis accommodation adults",
+            q: "crisis accommodation males adults",
             age_groups: ["adult"],
             client_gender: ["m", "u"],
         });
