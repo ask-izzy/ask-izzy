bb9767ebd4d3e2238307169a04f8f6655255d0c5
robynk <robyn@infoxchange.net.au>
2015-12-29 14:14:19 +1100

    Removed extra space

--- src/pages/personalisation/LifeSkillsSubcategories.js
+++ src/pages/personalisation/LifeSkillsSubcategories.js
@@ -13,7 +13,7 @@ export default class LifeSkillsSubcategories extends BaseMultiQuestion {
             /* eslint-disable max-len */
             "Daily living": remove("life skills education").append("daily living skills"),
             "Personal": remove("life skills education").append("personal development")
-                .append("-vcal - grants"),
+                .append("-vcal -grants"),
             "School": remove("life skills education").append("government schools")
             "Training with support": remove("life skills education")
                 .append("supported vocational training"),
