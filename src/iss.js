cb4c35994486eac5137c4bd6bce44777bf04ee8c
Daniel Heath <daniel@heath.cc>
2016-01-07 14:59:29 +1100

    Use the right magic character

--- src/iss.js
+++ src/iss.js
@@ -396,7 +396,7 @@ export async function search(
     let request_: searchRequest = {
         q: "",
         type: "service",
-        minimum_must_match: "3>50%",
+        minimum_must_match: "3<50%",
         limit: 10,
     };
 
