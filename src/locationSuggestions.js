6cdd4534dc121bd622ef2c267214b4dc9d86d485
Daniel Heath <daniel@heath.cc>
2016-01-11 17:24:13 +1100

    Add suffixes for major cities so that users search CBD locations by default

--- src/locationSuggestions.js
+++ src/locationSuggestions.js
@@ -4,6 +4,16 @@ import _ from "underscore";
 import Maps from "./maps";
 import storage from "./storage";
 
+type LocationCompletion = {
+    "suburb": string,
+    "state": string,
+}
+
+type GoogleCompletion = {
+    "types": Array<string>,
+    "terms": Array<{value: string}>,
+}
+
 /**
  * Take a search string and (optionally) the user's current location
  * and return a promise for an array of possible matches.
@@ -15,7 +25,7 @@ import storage from "./storage";
 export default async function suggest(
     input: string,
     location: ?Object,
-): Promise<Array<Object>> {
+): Promise<Array<LocationCompletion>> {
     const maps = await Maps();
     let request: AutocompletionRequest = {
         input: input,
@@ -35,15 +45,18 @@ export default async function suggest(
 
     let completions = await maps.autocompletePlaces(request);
 
+    // flow:disable no generators
     return [
         /*::{_:`*/
-        for (completion of filterCompletions(completions))
+        for (completion of injectSuffixes(filterCompletions(completions)))
             completion
         /*::`}*/
     ];
 }
 
-function *filterCompletions(completions: Array<Object>): Iterable<Object> {
+function *filterCompletions(
+    completions: Iterable<GoogleCompletion>
+): Iterable<LocationCompletion> {
     for (const completion of completions) {
         if (_.contains(completion.types, "locality")) {
             yield {
@@ -56,5 +69,62 @@ function *filterCompletions(completions: Array<Object>): Iterable<Object> {
                 suburb: completion.terms[1].value,
             }
         }
+    }
+}
+
+const majorCitySuffixes = {
+    "Sydney, New South Wales": "CBD",
+    "Melbourne, Victoria": "CBD",
+    "Brisbane, Queensland": "CBD",
+    "Perth, Western Australia": "CBD",
+    "Adelaide, South Australia": "CBD",
+    "Newcastle, New South Wales": "City",
+    "Canberra, Australian Capital Territory": "City",
+    "Wollongong, New South Wales": "City",
+    "Hobart, Tasmania": "CBD",
+    "Geelong, Victoria": "City",
+    "Townsville, Queensland": "City",
+    "Cairns, Queensland": "City",
+    "Darwin, Northern Territory": "City",
+    "Toowoomba, Queensland": "City",
+}
+
+function *injectSuffixes(
+    completions: Iterable<LocationCompletion>
+): Iterable<LocationCompletion> {
+    // flow:disable no generators
+    const completionArray: Array<LocationCompletion> = [
+        /*::{_:`*/
+        for (completion of completions)
+            completion
+        /*::`}*/
+    ];
+
+    for (const completion of completionArray) {
+        const suffix = majorCitySuffixes[
+            `${completion.suburb}, ${completion.state}`
+        ]
+
+        if (suffix) {
+            const extra = {
+                suburb: `${completion.suburb} ${suffix}`,
+                state: completion.state,
+            };
+
+            // If this version shows up later, remove it.
+            const existingExtra = _(completionArray).find(({suburb, state}) =>
+                (suburb == extra.suburb) && (state == extra.state)
+            );
+
+            if (existingExtra) {
+                completionArray.splice(
+                    completionArray.indexOf(existingExtra),
+                    1
+                );
+            }
+
+            yield extra;
+        }
+        yield completion;
     }
 }
