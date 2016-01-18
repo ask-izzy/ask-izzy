9a739d6a285a5328c9134dff0a57261448a1fe3b
Isaac Williams <isaac.williams@navydesign.com.au>
2016-01-18 15:49:50 +1100

    add terms of use link to footer

--- src/components/BrandedFooter.js
+++ src/components/BrandedFooter.js
@@ -11,15 +11,18 @@ class BrandedFooter extends React.Component {
     render(): ReactElement {
         return (
             <footer className="branding-footer-container">
-                <div className="about">
-                    <Link to="/about">About Ask Izzy</Link>
+                <div>
+                    <ul className="footer-list">
+                        <li><Link to="/about">About Ask Izzy</Link></li>
+                        <li><Link to="/terms">Terms of use</Link></li>
+                    </ul>
                 </div>
                 <div>
-                    Supported by <ul className="supporters">
-                        <li className="supporter">Infoxchange</li>
-                        <li className="supporter">Google</li>
-                        <li className="supporter">REA Group</li>
-                        <li className="supporter">News Corp Australia</li>
+                    Supported by <ul className="footer-list">
+                        <li>Infoxchange</li>
+                        <li>Google</li>
+                        <li>REA Group</li>
+                        <li>News Corp Australia</li>
                     </ul>
                 </div>
             </footer>
