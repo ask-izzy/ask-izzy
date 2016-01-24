c2770292f5fe19ed0b3b25ed77a267da844af482
robynk <robyn@infoxchange.net.au>
2016-01-25 10:11:06 +1100

    Changed transport keywords

--- src/pages/personalisation/EverydayThingsSubcategories.js
+++ src/pages/personalisation/EverydayThingsSubcategories.js
@@ -11,7 +11,7 @@ export default class EverydayThingsSubcategories extends BaseQuestion {
         answers: {
             /* eslint-disable max-len */
             "Food packages/parcels/vouchers": append("food parcel voucher pack"),
-            "Transport": append("public transport voucher petrol taxi subsidy"),
+            "Transport": append("transport voucher petrol taxi subsidy"),
             "Keeping warm": remove("material aid")
                 .append("swags blankets"),
             "Clothes": remove("material aid").append("clothes"),
