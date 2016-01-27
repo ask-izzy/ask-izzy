f9345ca0f262c83daf6c13dd29fc6e6af62bc92f
Daniel Heath <daniel@heath.cc>
2016-01-27 14:40:02 +1100

    Use more idiomatic code

--- src/google-tag-manager.js
+++ src/google-tag-manager.js
@@ -2,9 +2,7 @@
 
 export default function push(event: Object): void {
     if (typeof window != "undefined") {
-        if (!window.dataLayer) {
-            window.dataLayer = [];
-        }
+        window.dataLayer = window.dataLayer = [];
         window.dataLayer.push(event);
     }
 }
