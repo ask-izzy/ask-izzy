7b3bb13fa81057a3aaf0dfb0bd59ea53d136acbe
Daniel Heath <daniel@heath.cc>
2016-01-28 12:43:05 +1100

    Tidy up age code

--- src/pages/personalisation/Age.js
+++ src/pages/personalisation/Age.js
@@ -12,18 +12,10 @@ export default class Age extends BaseQuestion {
             "25 or younger": append({
                 q: "youth",
             }),
-            "26 to 39": append({
-                q: "",
-            }),
-            "40 to 54": append({
-                q: "",
-            }),
-            "55 to 64": append({
-                q: "",
-            }),
-            "65 or older": append({
-                q: "",
-            }),
+            "26 to 39": append(""),
+            "40 to 54": append(""),
+            "55 to 64": append(""),
+            "65 or older": append(""),
         },
     };
 
