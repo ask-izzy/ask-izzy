46616e8fccc3a0fdd3bcb67e716c337fcab79b33
Daniel Heath <daniel@heath.cc>
2016-02-02 16:47:32 +1100

    Extract var setting to their own file

--- src/server/index.js
+++ src/server/index.js
@@ -4,25 +4,9 @@ import path from "path";
 import express from "express";
 import render from "./render";
 
-declare var GLOBAL: Object;
+import loadVars from "./vars";
 
-if (!process.env.ISS_URL) {
-    console.error("Must set ISS_URL");
-    process.exit(1);
-}
-
-if (!process.env.GOOGLE_API_KEY) {
-    console.warn("GOOGLE_API_KEY not set");
-}
-
-// On the client, we set these variables on `window`
-// Making them globals on the server means the same
-// code can be used.
-GLOBAL.ISS_URL = process.env.ISS_URL;
-GLOBAL.GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
-GLOBAL.SITE_DOMAIN = process.env.SITE_DOMAIN;
-GLOBAL.GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID;
-GLOBAL.GOOGLE_TAG_MANAGER_ID = process.env.GOOGLE_TAG_MANAGER_ID;
+loadVars();
 
 // Initialize express server
 const server = express();
