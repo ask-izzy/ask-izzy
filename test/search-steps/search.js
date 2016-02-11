65df32d2408a068434b5f81a8d1d2da4d831126f
Daniel Heath <daniel@heath.cc>
2016-02-11 16:06:06 +1100

    Add some more search tests

--- test/search-steps/search.js
+++ test/search-steps/search.js
@@ -43,6 +43,13 @@ function setDemographicsNone(): Promise<void> {
     return setDemographics.bind(this)([]);
 }
 
+async function setSubcategoryItem(
+    category: string,
+    item: string,
+): Promise<void> {
+    storage.setItem(`sub-${category}`, item);
+}
+
 async function setSubcategoriesNone(
     items: Array<string>,
 ): Promise<void> {
@@ -58,8 +65,26 @@ async function setSubcategoriesNone(
     storage.setItem("sub-technology", "(skipped)");
 }
 
-async function setAgeTo(age: string): Promise<void> {
-    storage.setItem("age", age);
+async function setAge(age: number): Promise<void> {
+    let description = "25 or younger";
+
+    if (age >= 26) {
+        description = "26 to 39";
+    }
+
+    if (age >= 40) {
+        description = "40 to 54";
+    }
+
+    if (age >= 55) {
+        description = "55 to 64";
+    }
+
+    if (age >= 65) {
+        description = "65 or older";
+    }
+
+    storage.setItem("age", description);
 }
 
 // Shamelessly copied from ResultsPage
@@ -94,7 +119,7 @@ function objectMatches(expectation: any, subject: any): boolean {
 }
 
 async function searchIss(categoryName: string): Promise<searchResults> {
-    const category = Categories.find(({key}) => key == "housing")
+    const category = Categories.find(({key}) => key == categoryName)
     const request = issRequest(category);
 
     if (!request) {
@@ -103,11 +128,14 @@ async function searchIss(categoryName: string): Promise<searchResults> {
         } to generate a query`);
     }
 
-    return await search(request);
+    return await search(Object.assign(request, {limit: 50}));
 }
 
-async function assertNoSuchResults(table: Array<Object>): Promise<void> {
-    let services = (await searchIss("housing")).objects;
+async function assertNoSuchResults(
+    category: string,
+    table: Array<Object>
+): Promise<void> {
+    let services = (await searchIss(category)).objects;
 
     for (const banned of table) {
         services.forEach((service, index) => {
@@ -122,19 +150,25 @@ async function assertNoSuchResults(table: Array<Object>): Promise<void> {
     }
 }
 
-async function assertResults(expectedResults: Array<Object>): Promise<void> {
-    let services = (await searchIss("housing")).objects;
+async function assertResults(
+    category: string,
+    expectedResults: Array<Object>
+): Promise<void> {
+    let services = (await searchIss(category)).objects;
 
-    let missing = expectedResults.filter(
+    let found = expectedResults.filter(
         (requiredAttrs) =>
-            !services.find((service) =>
+            services.find((service) =>
                 objectMatches(requiredAttrs, service))
     )
+    let missing = _.difference(expectedResults, found)
 
     if (missing.length > 0) {
         throw new Error(`Expected to find ${
             JSON.stringify(missing, null, 4)
-        } results but they were absent.`);
+        } results but they were absent. ${
+            JSON.stringify(found.map(service => service.id), null, 4)
+        } services did match.`);
     }
 }
 
@@ -143,15 +177,21 @@ module.exports = (function() {
         .given("I have deleted all answers", unpromisify(deleteAnswers))
         .given('my location is "$STRING"', unpromisify(setLocation))
         .given("I have (somewhere|nowhere) to sleep tonight",
-               unpromisify(setSleepTonight))
-        .given("I am 17 years old",
-               unpromisify(_.partial(setAgeTo, "25 or younger")))
+            unpromisify(setSleepTonight))
+        .given("I am $NUMBER years old",
+            unpromisify(setAge))
         .given("I am not part of any relevant demographics",
-               unpromisify(setDemographicsNone))
+            unpromisify(setDemographicsNone))
+        .given("I am part of the following demographics\n$lines",
+            unpromisify(setDemographics))
         .given("I am not interested in any subcategory",
-               unpromisify(setSubcategoriesNone))
-        .then("my results for housing should not contain\n$yaml",
+            unpromisify(setSubcategoriesNone))
+        .given("I need the following for $STRING: $string",
+               unpromisify(setSubcategoryItem))
+        .then("my results for $STRING should not contain\n$yaml",
             unpromisify(assertNoSuchResults))
-        .then("my results for housing should contain\n$yaml",
+        .then("my results for $STRING should contain\n$yaml",
             unpromisify(assertResults))
+        .then("my results for $STRING would ideally contain\n$yaml",
+            unpromisify(assertNoSuchResults))
 })();
