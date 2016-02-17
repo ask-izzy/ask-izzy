2b6ce3e782482895b5d5b08c269afad57112a358
Daniel Heath <daniel@heath.cc>
2016-02-17 17:33:28 +1100

    Remove adjustments added due to misunderstanding (backwards) the DSL

--- src/pages/personalisation/HousingSubcategories.js
+++ src/pages/personalisation/HousingSubcategories.js
@@ -11,33 +11,18 @@ export default class HousingSubcategories extends BaseQuestion {
         question: "Which situation is most like yours?",
         answers: {
             "On the street": remove("housing")
-                .remove("-(respite care)")
-                .remove("-(housing information)")
-                .remove("-hef")
                 .append("crisis accommodation"),
             "Couch surfing": remove("housing")
                 .append("homeless accommodation"),
             "In a rooming house": remove("housing")
-                .remove("-(respite care)")
-                .remove("-(housing information)")
-                .remove("-hef")
                 .append("community housing -(rooming house)"),
             "Private rental": remove("housing")
-                .remove("-(respite care)")
-                .remove("-(housing information)")
-                .remove("-hef")
                 .append("transitional accommodation"),
             "Public housing": remove("housing")
-                .remove("-(respite care)")
-                .remove("-(housing information)")
-                .remove("-hef")
                 .append("social housing")
                 .append("-(public rental)")
                 .append("-(public housing)"),
             "Mortgaged housing": remove("housing")
-                .remove("-(respite care)")
-                .remove("-(housing information)")
-                .remove("-hef")
                 .append("transitional accommodation"),
         },
     };
