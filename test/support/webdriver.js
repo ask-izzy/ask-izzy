9819a95dbe94219c3ff33ffeb61a3d22cc79756e
Daniel Heath <daniel@heath.cc>
2016-02-18 11:35:15 +1100

    Remove travis CI config

--- test/support/webdriver.js
+++ test/support/webdriver.js
@@ -57,48 +57,10 @@ export function gotoUrl(driver: Webdriver.WebDriver, url: string): Promise {
  */
 export default async function webDriverInstance(
 ): Promise<Webdriver.WebDriver> {
-    const branch = process.env.TRAVIS_BRANCH || "Manual";
-    const baseCaps: Webdriver.Capabilities = {
-        username: process.env.SAUCE_USERNAME,
-        accessKey: process.env.SAUCE_ACCESS_KEY,
-        name: `Ask Izzy ${branch} ${process.env.TRAVIS_PULL_REQUEST || ""}`,
-        tags: [
-            process.env.TRAVIS_PULL_REQUEST || "Manual",
-            branch,
-        ],
-        screenResolution: "1024x768",
-        captureHtml: true,
-        timeZone: "Melbourne",
-        public: "public",
-        build: process.env.TRAVIS_BUILD_NUMBER || "Manual",
-        tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
-    };
-
-    if (process.env.SELENIUM_DEVICE) {
-        baseCaps.appiumVersion = "1.4.10";
-        baseCaps.deviceName = process.env.SELENIUM_DEVICE;
-
-        // From selenium-webdriver - we need to
-        // fill in some extra fields for appium
-        const browser = (process.env.SELENIUM_BROWSER || "").split(/:/, 3);
-
-        /* flow:disable unsupported by flow */
-        [_, baseCaps.platformVersion, baseCaps.platformName] = browser;
-
-        baseCaps.emulator = true;
-    }
-
-    if (process.env.SELENIUM_ORIENTATION) {
-        baseCaps.deviceOrientation = process.env.SELENIUM_ORIENTATION;
-    }
-
+    // Remove version from browserName (not supported)
     const browserName = (process.env.SELENIUM_BROWSER || "").split(/:/)[0];
 
     const driver = new Webdriver.Builder()
-        /* These are used by Sauce Labs
-         * You should also pass SELENIUM_REMOTE_URL to connect
-         * via Selenium Grid */
-        .withCapabilities(baseCaps)
         /* Default to using phantom.js if `SELENIUM_BROWSER` not provided. */
         .forBrowser(browserName || "phantomjs")
         .build();
