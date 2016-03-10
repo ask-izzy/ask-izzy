93a220a305ffa62fa0ed78f852e95d148a045c4d
Daniel Heath <daniel@heath.cc>
2016-03-10 11:48:53 +1100

    Add unit tests for descriptionSentences

--- test/unit/issTest.js
+++ test/unit/issTest.js
@@ -5,8 +5,46 @@
 
 import assert from "assert";
 import { mungeUrlQuery } from "../../src/iss";
+import Service from "../../fixtures/factories/Service";
 
 describe("iss service", function() {
+    describe("splitting description into sentences", function() {
+        function test(input: string, output: Array<string>): Function {
+            return function() {
+                const service = Service({description: input});
+
+                assert.deepEqual(
+                    service.descriptionSentences(),
+                    output
+                );
+            }
+        }
+
+        it("preserves URLs", test(
+            "See our site at www.foo.com. More details are there.",
+            [
+                "See our site at www.foo.com.",
+                "More details are there.",
+            ]
+        ));
+
+        it("ends sentences with a full stop", test(
+            "A description",
+            ["A description."]
+        ));
+
+        it("removes trailing whitespace", test(
+            "trailing whitespace    . it happens     .     ",
+            ["trailing whitespace.", "it happens."]
+        ));
+
+        it("removes leading whitespace", test(
+            "  leading whitespace.     also happens",
+            ["leading whitespace.", "also happens."]
+        ));
+
+    })
+
     describe("Munging URLs", function() {
 
         it("Preserves unrelated query params", function() {
