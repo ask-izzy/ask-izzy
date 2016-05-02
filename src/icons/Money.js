482fe853d434667ab0629cf11e9660baca7c2b7f
Daniel Heath <daniel@heath.cc>
2016-05-02 11:12:35 +1000

    Update typechecker for icons

--- src/icons/Money.js
+++ src/icons/Money.js
@@ -6,11 +6,7 @@ import React from "react";
 import classnames from "classnames";
 
 export default class SvgIconMoney extends React.Component {
-    props: Object;
-    state: Object;
-    static propTypes = {
-        className: React.PropTypes.string,
-    };
+    props: {className?: string};
 
     render() {
         const {className, ...rest} = this.props;
