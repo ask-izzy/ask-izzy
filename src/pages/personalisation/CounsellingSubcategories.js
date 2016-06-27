3afa40688e64da779f69bc763068eeff7a5bed3e
Daniel Heath <daniel@heath.cc>
2016-06-27 11:12:12 +1000

    Remove 'dead' string modifications

--- src/pages/personalisation/CounsellingSubcategories.js
+++ src/pages/personalisation/CounsellingSubcategories.js
@@ -11,17 +11,13 @@ export default class CounsellingSubcategories extends BaseQuestion {
         question: "What issues are you dealing with?",
         answers: {
             "Homelessness": append("housing"),
-            "Family violence": remove("support")
-                .append("family violence"),
+            "Family violence": append("family violence"),
             "Sexual identity": append("sexuality"),
-            "Family or relationships": remove("support")
-                .append("family relationship"),
-            "Money matters": remove("support counselling")
+            "Family or relationships": append("family relationship"),
+            "Money matters": remove("counselling")
                 .append("(money matters)"),
-            "Gambling": remove("support")
-                .append("gambling"),
-            "Find online counselling": remove("support")
-                .append("online"),
+            "Gambling": append("gambling"),
+            "Find online counselling": append("online"),
             "Other": append(""),
         },
     };
