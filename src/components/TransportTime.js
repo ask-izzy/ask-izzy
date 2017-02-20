148cd9bb509dd02b2f4be4beaecc9fc046c1576d
Michael Adams <michaela@infoxchange.org>
2017-02-21 10:56:39 +1100

    Add key attribute to travel-time element, as each child in an iterator should have a unique key prop #134181

--- src/components/TransportTime.js
+++ src/components/TransportTime.js
@@ -109,7 +109,7 @@ class TransportTime extends React.Component {
             }
 
             return (
-                <div className="travel-time">
+                <div className="travel-time" key={key}>
                     {icon}
                     <time dateTime={arrivalTime.toISOString().slice(0, -1)}>
                     {
