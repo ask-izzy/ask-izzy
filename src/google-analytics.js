9aab7a816e3464ff03461d50ae5d079ed4449493
Michael Adams <michaela@infoxchange.org>
2017-03-31 16:13:26 +1100

    Replace GA async snippet. #131924

--- src/google-analytics.js
+++ src/google-analytics.js
@@ -5,7 +5,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
     if (process.env.NODE_ENV == "production") {
 
         // Analytics
-        window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','/static/analytics.js','ga');
         ga('create', window.GOOGLE_ANALYTICS_ID, 'auto');
         ga('set', 'anonymizeIp', true);  // Anonymize IP addresses of all users.
 
