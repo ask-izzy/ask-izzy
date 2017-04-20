68b19a02c66fb6ce08c9edf3cc4daab9af29ffc3
Michael Adams <michaela@infoxchange.org>
2017-04-20 11:12:19 +1000

    Add config dir to linter and fix lint errors

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,7 @@
 /* @flow */
 // App config the for development environment.
 // Do not require this directly. Use ./src/config instead.
+
 export default {
-	siteMail : 'example@example.com',
+    siteMail: 'example@example.com',
 };
