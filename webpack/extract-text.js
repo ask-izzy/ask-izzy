f5c859047bd36d7f883b05f56442a2dcb2bdb3c7
pchan <pchan@infoxchange.org.au>
2017-10-16 14:04:45 +1100

    Update extract-text to use contenthash

--- webpack/extract-text.js
+++ webpack/extract-text.js
@@ -4,7 +4,7 @@ import ExtractTextPlugin from "extract-text-webpack-plugin";
 import cssLoaders from "./css-loaders";
 
 const cssExtractor = new ExtractTextPlugin({
-    filename: "[name]-[chunkhash].css",
+    filename: "[name]-[contenthash].css",
     allChunks: true,
 });
 
