f5bf9ddf7d1a0f818e906558662427ea785dde5a
christine <christine@infoxchange.org>
2018-07-31 13:30:34 +1000

    Add icon for Domestic and family violence

--- src/icons/index.js
+++ src/icons/index.js
@@ -30,6 +30,7 @@ import SvgIconDemographicTorresStrait from "./DemographicTorresStrait.js";
 import SvgIconDemographicVeteran from "./DemographicVeteran.js";
 import SvgIconEmail from "./Email.js";
 import SvgIconEntertainment from "./Entertainment.js";
+import SvgIconEscapeViolence from "./EscapeViolence.js";
 import SvgIconExternalLink from "./ExternalLink.js";
 import SvgIconFacilities from "./Facilities.js";
 import SvgIconFeedback from "./Feedback.js";
@@ -97,6 +98,7 @@ export default {
     EscapeViolence: SvgIconEscapeViolence,
     Email: SvgIconEmail,
     Entertainment: SvgIconEntertainment,
+    EscapeViolence: SvgIconEscapeViolence,
     ExternalLink: SvgIconExternalLink,
     Facilities: SvgIconFacilities,
     Feedback: SvgIconFeedback,
