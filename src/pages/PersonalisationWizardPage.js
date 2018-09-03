e99922e8cb6d4ae12141c49d34b12bac068f2a95
Mat Munn <mmunn@infoxchange.org>
2018-09-03 11:23:07 +1000

    Fixed navigation bug in Safari

--- src/pages/PersonalisationWizardPage.js
+++ src/pages/PersonalisationWizardPage.js
@@ -25,7 +25,12 @@ class PersonalisationWizardPage extends BasePersonalisationPage {
     }
 
     previousStep(): void {
-        super.previousStep();
+        if (this.refs.subpage && this.refs.subpage.onPreviousStep) {
+            if (!this.refs.subpage.onPreviousStep()) {
+                return;
+            }
+        }
+
         this.setState({nextDisabled: false});
 
         const prevSubPage = this.prevSubPage();
@@ -99,11 +104,7 @@ class PersonalisationWizardPage extends BasePersonalisationPage {
 
     get backMessage(): string {
         if (this.refs.subpage && this.refs.subpage.customTitle) {
-            const title = this.refs.subpage.customTitle();
-
-            if (title) {
-                return title;
-            }
+            return this.refs.subpage.customTitle;
         }
 
         const prevPage = this.prevSubPage();
