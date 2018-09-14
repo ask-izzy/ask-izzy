5c7f194a0c1111150f76eeb6e957f93e1afe4478
Mat Munn <mmunn@infoxchange.org>
2018-09-14 15:39:23 +1000

    Create higher order component to abstract out mobile detection in order to avoid repetitive code  - Update BrandedFooter to use this new component Create base component (BaseLogoWithTextBox) for adding blocks on personalisation pages  - Change OnlineSafetyLink to extend from this base component  - Create DomesticViolenceLink which extends from this base component Update iconify script for new version of Flow Rebuild icons Create Using Violence and Under 18  DFV static pages

---
+++ src/pages/personalisation/Under18DomesticViolenceScreen.js
@@ -0,0 +1,69 @@
+/* @flow */
+
+import * as React from "react";
+import BaseStaticPersonalisation from "./BaseStaticPersonalisation";
+import icons from "../../icons";
+import AreYouSafe from "./AreYouSafe";
+import storage from "../../storage";
+import DomesticViolenceLink from "../../components/DomesticViolenceLink";
+import MobileDetect from "../../components/higherorder/MobileDetect";
+
+class Under18DomesticViolenceScreen extends BaseStaticPersonalisation {
+    static title = "";
+
+    static defaultProps = {
+        name: "under-18-dfv",
+        heading: "Under 18",
+        byline: "All of your answers are private and anonymous",
+        showBaseTextBox: true,
+        baseTextBoxComponent: <DomesticViolenceLink/>,
+    };
+
+    static summaryLabel = "Under 18 Domestic Violence Information";
+
+    static showPage(): boolean {
+        return true;
+    }
+
+    renderContent(): React.Element<any> {
+        const linkService = "/service/634190-1800respect";
+        const numberService = "1800 551 800";
+        const chatService = "https://kidshelpline.com.au/";
+
+        const linkWoah = "https://woah.org.au/";
+        const linkYoungAndEsafe = "https://www.esafety.gov.au/youngandesafe";
+
+        return (
+            <div className="AreYouSafe">
+                <div className="safety-message">
+                    <icons.PhoneTalk className="PhoneIcon" />
+                    <h2>
+                        Violence is never OK. It may be happening at home
+                        with family or with someone you're dating.
+                    </h2>
+                    <h3>
+                        Call <a href={linkService}>Kid's Helpline</a> on{' '}
+                        {
+                            this.props.mobileView ? (
+                                <a href={`tel:${numberService}`}>
+                                    {numberService}
+                                </a>
+                            ) : (
+                                `${ numberService }`
+                            )
+                        }
+                        {' '} or chat online <a href={chatService}>here</a>
+                    </h3>
+                    <h3>
+                        Visit <a href={linkWoah}>WOAH</a> or{' '}
+                        <a href={linkYoungAndEsafe}>Young and E-SAFE</a> to
+                        learn more
+                    </h3>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default MobileDetect(Under18DomesticViolenceScreen);
