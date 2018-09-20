96084c3afe143600e1a1400156d5f31651eebd65
Mat Munn <mmunn@infoxchange.org>
2018-09-20 15:35:06 +1000

    Fix lint errors

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,13 @@
+/* @flow */
+
 import storage from "../storage";
 
 import AreYouSafe from "../pages/personalisation/AreYouSafe";
 import OnlineSafetyScreen from "../pages/personalisation/OnlineSafetyScreen";
 
 export const resetDfvOptions = (): void => {
-    if (Boolean(AreYouSafe.answer) && [
+    if (Boolean(AreYouSafe.answer) &&
+        [
             "No",
             "I'm not sure",
         ].indexOf(AreYouSafe.answer) > -1 &&
