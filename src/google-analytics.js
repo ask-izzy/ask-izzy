a4f3c0b9d9c548ab54aab6a3c44c58be505b6956
Michael Adams <michaela@infoxchange.org>
2019-01-04 15:23:13 +1100

    Add version to end of analytics.js file to cachebust. refs ts/dev/askizzy#136

--- src/google-analytics.js
+++ src/google-analytics.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','/static/analytics.js','ga');
+        })(window,document,'script','/static/analytics-' + window.VERSION + '.js','ga');
         ga('create', window.GOOGLE_ANALYTICS_ID, 'auto');
         ga('set', 'anonymizeIp', true);  // Anonymize IP addresses of all users.
 
