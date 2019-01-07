70839a3372076691679b4dcdc3b5b872dbae63eb
Michael Adams <michaela@infoxchange.org>
2019-01-08 10:57:58 +1100

    Remove unused google-maps-api.js file

--- invoke.sh
+++ invoke.sh
@@ -51,7 +51,6 @@ case "$1" in
         set -x # Logs
         ./script/generate-env-vars > /static/env-$(cat public/VERSION).js
         ./script/build-gmaps-file
-        cp ./public/static/google-maps-api.js ./public/static/google-maps-api-$(cat public/VERSION).js
         cp ./public/static/scripts/request-interceptor.js ./public/static/scripts/request-interceptor-$(cat public/VERSION).js
         cp -r ./public/static/* /static/
         ;;
