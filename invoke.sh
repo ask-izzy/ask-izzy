cbac6d74b92375b2de0e86239febee07d45c1976
Donald Mu <dmu@infoxchange.org>
2019-01-25 10:53:34 +1100

    Add csp frame-ancestors headers to nginx

--- invoke.sh
+++ invoke.sh
@@ -53,6 +53,8 @@ case "$1" in
         ./script/build-gmaps-file
         cp ./public/static/scripts/request-interceptor.js ./public/static/scripts/request-interceptor-$(cat public/VERSION).js
         cp -r ./public/static/* /static/
+	# create nginx conf
+	echo "$(envsubst '${CSP_FRAME_ANCESTORS}' < conf/nginx.conf)" > conf/nginx.conf
         ;;
 
     serve)
@@ -64,6 +66,10 @@ case "$1" in
     env)
         shift 1
         exec env $@
+        ;;
+
+    shell)
+        /bin/bash
         ;;
 
     *)
