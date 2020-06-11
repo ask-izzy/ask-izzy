8757b1edc424f1a74adce73d5a971b0f5431457a
flara <flara@infoxchange.org>
2020-06-11 10:30:12 +1000

    Add Apollo client and query component for retrieving graphql data

--- src/pages/BasePage.js
+++ src/pages/BasePage.js
@@ -5,6 +5,9 @@ import Helmet from "react-helmet";
 import DocumentTitle from "react-document-title";
 import { makeTitle } from "../routes";
 
+import { ApolloProvider } from "react-apollo";
+import client from "../utils/apolloClient";
+
 type Props = {
     main: any,
     children: any,
@@ -26,30 +29,32 @@ export default class BasePage extends React.Component<Props, void> {
         const canonicalUrl = `https://askizzy.org.au${this.props.location.pathname}`;
 
         return (
-            <div className="BasePage">
+            <ApolloProvider client={client}>
+                <div className="BasePage">
 
-                <Helmet>
-                    <link
-                        rel="canonical"
-                        content={canonicalUrl}
-                    />
-                    <meta
-                        property="og:url"
-                        content={canonicalUrl}
-                    />
-                </Helmet>
+                    <Helmet>
+                        <link
+                            rel="canonical"
+                            content={canonicalUrl}
+                        />
+                        <meta
+                            property="og:url"
+                            content={canonicalUrl}
+                        />
+                    </Helmet>
 
-                <DocumentTitle
-                    title={makeTitle(
-                        this.props.routes,
-                        this.props.params
-                    )}
-                />
-                <main>
-                    {this.props.main || this.props.children}
-                </main>
-                {this.props.footer}
-            </div>
+                    <DocumentTitle
+                        title={makeTitle(
+                            this.props.routes,
+                            this.props.params
+                        )}
+                    />
+                    <main>
+                        {this.props.main || this.props.children}
+                    </main>
+                    {this.props.footer}
+                </div>
+            </ApolloProvider>
         );
     }
 }
---
+++ src/queries/content/covidBanner.js
@@ -0,0 +1,13 @@
+import gql from "graphql-tag";
+
+const bannerQuery = gql`
+query Banner($id: ID!) {
+  banner(id: $id) {
+    id
+    Description
+    Body
+  }
+}
+`;
+
+export default bannerQuery;
