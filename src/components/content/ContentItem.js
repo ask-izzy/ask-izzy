2136c4b89dbe467342629a6619ec7f107b3eb85e
Michael Adams <michaela@infoxchange.org>
2020-06-30 17:19:09 +1000

    Adding content components. Use CMS query for key information content on covid page.

---
+++ src/components/content/ContentItem.js
@@ -0,0 +1,45 @@
+/* @flow */
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+type Props = {
+    id: Number,
+    title : String,
+    subtitle?: String,
+    link?: String,
+};
+
+class ContentItem extends React.Component<Props> {
+
+    render() {
+        return (
+            <li
+                key={this.props.id}
+                className="keyInfo"
+            >
+                <h3>
+                    <Link
+                        className="learnMore"
+                        to={this.props.link}
+                        rel="noopener noreferer"
+                    >
+                        {this.props.title}
+                    </Link>
+                </h3>
+                {this.props.subtitle ? (<h4>{this.props.subtitle}</h4>) : null}
+                <div className="body">{this.props.body}</div>
+                <Link
+                    className="learnMore"
+                    to={this.props.link}
+                    rel="noopener noreferer"
+                >
+                    Learn More
+                </Link>
+            </li>
+        );
+    }
+
+}
+
+export default ContentItem;
