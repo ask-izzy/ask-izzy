72300fc674d9d0ede3a6a72d9794fa34e08e9e3c
Michael Adams <michaela@infoxchange.org>
2020-07-02 14:12:54 +1000

    Add category param into external resource gql query

--- src/queries/content/externalResources.js
+++ src/queries/content/externalResources.js
@@ -3,13 +3,17 @@
 import gql from "graphql-tag"
 
 const externalResourcesQuery = gql`
-query ExternalResources {
-  externalResources {
+query ExternalResources($category: [String]) {
+  externalResources(where: {categories:{Name_in:$category}}) {
     id,
     Title,
     Body,
     Author,
     Link,
+    categories {
+      id,
+      Name,
+    }
   }
 }
 `
