8f0371e60563140671685368a96783d22c223691
Michael Adams <michaela@infoxchange.org>
2020-08-04 15:42:55 +1000

    Add feedback forms to results and service pages
    - Also load Usersnap feedback tool

--- src/analytics.js
+++ src/analytics.js
@@ -3,6 +3,15 @@
 import 'url-search-params-polyfill';
 
 document.addEventListener("DOMContentLoaded", function(event) {
+
+    window.onUsersnapCXLoad = function(api) {
+        api.init();
+    }
+    var script = document.createElement('script');
+    script.defer = 1;
+    script.src = 'https://widget.usersnap.com/global/load/e8e15b8e-a779-4356-85dd-7fcbb37bf738?onload=onUsersnapCXLoad';
+    document.getElementsByTagName('head')[0].appendChild(script);
+
     if (window.GOOGLE_TAG_MANAGER_ID && window.GOOGLE_TAG_MANAGER_AUTH && window.GOOGLE_TAG_MANAGER_ENV) {
         for (const index of window.GOOGLE_TAG_MANAGER_ID.split("~").keys()) {
             const gtmId = window.GOOGLE_TAG_MANAGER_ID.split("~")[index];
