d87c2711148c0f3d3c76057644adfe4a66b2250c
Callum Gare <cgare@infoxchange.org>
2020-08-11 13:34:35 +1000

    Add second GTM instance

--- src/contexts/router-context.js
+++ src/contexts/router-context.js
@@ -1,6 +1,8 @@
 /* @flow */
 import React from "react";
 import { withRouter } from "react-router";
+import routes from "../routes";
+import {Redirect, Switch} from "react-router-dom";
 
 const RouterContext = React.createContext<Object>();
 
@@ -12,7 +14,10 @@ export const InjectRouterContext = withRouter(
             router: {
                 history,
                 location,
-                match: matchedRoute,
+                match: {
+                    props: getRouteProps(matchedRoute),
+                    ...matchedRoute,
+                }
             },
         }
         return (
@@ -22,3 +27,25 @@ export const InjectRouterContext = withRouter(
         )
     }
 )
+
+
+
+function getRouteProps(routeMatch): Object {
+    // Routes are nested components so flatten out and extract just the props
+    // for each route 
+    function extractRouteInfo(routes): Array<Object> {
+        if (routes instanceof Array) {
+            return routes
+                .map(subRoutes => extractRouteInfo(subRoutes)).flat()
+        } else if (routes.type === Switch) {
+            return routes.props.children
+                .map(subRoutes => extractRouteInfo(subRoutes)).flat()
+        } else if (routes.props) {
+            return routes.props
+        }
+        return []
+    }
+    const routesInfo = extractRouteInfo(routes)
+
+    return routesInfo.find(route => route.path === routeMatch.path)
+}
