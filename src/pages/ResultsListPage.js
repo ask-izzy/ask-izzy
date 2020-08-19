82617331f66128c7355ef895b83e7b0245ff0bc5
Michael Adams <michaela@infoxchange.org>
2020-08-19 11:45:36 +1000

    Load more results -> See more results

--- src/pages/ResultsListPage.js
+++ src/pages/ResultsListPage.js
@@ -133,7 +133,7 @@ class ResultsListPage extends ResultsPage<> {
             return (
                 <ButtonListItem
                     className="MoreResultsButton"
-                    primaryText="Load more results…"
+                    primaryText="See more results"
                     onClick={this.loadNextSearchPage}
                     analyticsEvent={{
                         event: "Action Triggered - Load More Results",
