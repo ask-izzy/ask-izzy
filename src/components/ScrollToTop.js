d12b0310022715696e86819dc4d6540085c29ffa
Michael Adams <michaela@infoxchange.org>
2020-08-21 13:54:48 +1000

    Track history using hook to help with back button handling.

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,16 +1,28 @@
+/* @flow */
 import React, { useEffect, Fragment } from "react";
 import { withRouter } from "react-router-dom";
+import storage from "../storage";
 
 function ScrollToTop({ history, children }) {
-    useEffect(() => {
-        const unlisten = history.listen(() => {
-            window.scrollTo(0, 0);
-        });
+    let historyLength = storage.getHistoryLength();
 
-        return () => {
-            unlisten();
+    function setHistoryLength(newLength: number): void {
+        historyLength = newLength;
+        storage.setHistoryLength(newLength);
+    }
+
+    useEffect(() => history.listen(({location, action}) => {
+        window.scrollTo(0, 0);
+
+        if (action === "POP") {
+            setHistoryLength(historyLength - 1);
         }
-    }, []);
+
+        if (action === "PUSH") {
+            setHistoryLength(historyLength + 1);
+        }
+
+    }), [])
 
     return <Fragment>{children}</Fragment>;
 }
