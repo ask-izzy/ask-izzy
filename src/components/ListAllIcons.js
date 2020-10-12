03be05f45dbe0a5a772e40193c1c9ce64a87a64a
Callum Gare <cgare@infoxchange.org>
2020-10-12 20:51:20 +1100

    Show if component is unrenderable in style guide

---
+++ src/components/ListAllIcons.js
@@ -0,0 +1,26 @@
+/* @flow */
+
+import React from "react";
+import icons from "../icons";
+
+// This component is so icons can be viewed in the style guide
+// http://localhost:8000/styleGuide/component/ListAllIcons
+
+class ListAllIcons extends React.Component<void, void> {
+    static sampleProps = {default: {}};
+
+    render() {
+        return (
+            <ul>{Object.entries(icons).map(
+                ([iconName, Icon]) =>
+                <li key={iconName}>
+                    <Icon className="big" />
+                    <br />
+                    <span>{iconName}</span>
+                </li>
+            )}</ul>
+        );
+    }
+}
+
+export default ListAllIcons;
