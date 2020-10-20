dbd5c79afd6b8c9e468c36745e37291b13bdc677
Callum Gare <cgare@infoxchange.org>
2020-10-20 21:39:49 +1100

    Update results page content

--- src/components/ViewOnMapButton.js
+++ src/components/ViewOnMapButton.js
@@ -3,27 +3,9 @@
 import React from "react";
 import icons from "../icons";
 import LinkListItem from "./LinkListItem";
-import maps, {MapsApi} from "../maps";
 
-type State = {
-    maps: ?MapsApi
-}
-
-export default class ViewOnMapButton extends React.Component<any, State> {
-    constructor() {
-        super();
-        this.state = {maps: null};
-    }
-
-    componentDidMount(): void {
-        maps().then(maps => this.setState({maps}));
-    }
-
+export default class ViewOnMapButton extends React.Component<{}> {
     render() {
-        if (!this.state.maps) {
-            return null;
-        }
-
         return (
             <LinkListItem
                 className="ViewOnMapButton"
