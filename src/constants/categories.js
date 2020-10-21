c2b0ea39b03efb0106bfee0bd36399a1e44e8164
Callum Gare <cgare@infoxchange.org>
2020-10-21 14:37:15 +1100

    Update job category name

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -82,8 +82,8 @@ const categories = [
         ],
     }),
     new Category({
-        title: "Jobs and training",
-        subtitle: "words go here",
+        title: "Jobs, training and skill development",
+        subtitle: "Finding work, volunteering and training",
         slug: "jobs-and-training",
         icon: icons.Job,
         bannerImage: 'finding-work',
