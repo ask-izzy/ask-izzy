95bf889f374aa15431dff34060fe223e28c4b694
Callum Gare <cgare@infoxchange.org>
2020-10-21 14:36:57 +1100

    Design tweaks

--- src/pages/personalisation/HealthSubcategories.js
+++ src/pages/personalisation/HealthSubcategories.js
@@ -15,10 +15,10 @@ export default class HealthSubcategories extends BaseQuestion {
             "I want to find someone to talk to right now":
                 append("(mental health) helpline lifeline -(coordinating bodies)")
                 .append({catchment: "prefer"}),
-            "I want to find websites or services that can help me when I need it":
+            "I want to find website or online services that can help me":
                 append("counselling online -(coordinating bodies)")
                 .append({catchment: "prefer"}),
-            "I just want to see what's available":
+            "I just want to see what's available for when I need help":
                 append("counselling -(coordinating bodies) -assault")
         }
     };
