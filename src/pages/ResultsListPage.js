0691326956eefb564ddaeacb38c2380b5095d700
Callum Gare <cgare@infoxchange.org>
2020-11-04 17:56:14 +1100

    Add query load callback to remove lag showing ToC

--- src/pages/ResultsListPage.js
+++ src/pages/ResultsListPage.js
@@ -53,26 +53,11 @@ class ResultsListPage extends ResultsPage<Props, ExtraState> {
     constructor(props: Object, context: Object) {
         super(props, context);
 
-        this.primaryInfoEl = null;
-        this.keyInfoEl = null;
-
         this.state = {
             ...this.state,
             hasInfo: false,
             hasTools: false,
         };
-    }
-
-    keyInfoEl: any;
-    primaryInfoEl: any;
-
-    componentDidUpdate = (prevProps: Object, prevState: Object) => {
-        if (this.primaryInfoEl && !prevState.hasTools) {
-            this.setState({ hasTools: true })
-        }
-        if (this.keyInfoEl && !prevState.hasInfo) {
-            this.setState({ hasInfo: true })
-        }
     }
 
     recordMapClick(): void {
@@ -89,7 +74,22 @@ class ResultsListPage extends ResultsPage<Props, ExtraState> {
                 location: storage.getLocation(),
             });
         }
+    }
 
+    get CMSCategoryName(): ?string {
+        const category = this.category
+        if (!category) {
+            return null
+        }
+        let catName = category.title
+        const subCategory = category.slug === "money" &&
+            storage.getItem("sub-money")
+
+        if (category.slug === "money" && subCategory === "Centrelink") {
+            catName = "Centrelink"
+        }
+
+        return catName
     }
 
     render() {
@@ -171,13 +171,16 @@ class ResultsListPage extends ResultsPage<Props, ExtraState> {
     )
 
     renderPrimaryInfo() {
+        if (!this.category) {
+            return null
+        }
         return (
             <Query
                 query={externalResourcesQuery}
                 loadingComponent={this.renderLoadingComponent()}
                 args={
                     {
-                        "category": [this.category.title],
+                        "category": [this.CMSCategoryName],
                         "tag": [
                             "Tool",
                         ],
@@ -186,24 +189,21 @@ class ResultsListPage extends ResultsPage<Props, ExtraState> {
                         ],
                     }
                 }
+                dataLoadedCallback={data =>
+                    data.externalResources.length > 0 &&
+                        this.setState({ hasTools: true })
+                }
             >
-                {data => {
-                    if (data.data.externalResources.length > 0) {
+                {({data}) => {
+                    if (data.externalResources.length > 0) {
                         return (<React.Fragment>
                             <a className="anchor"
                                 id="tools"
                             />
-                            <div
-                                className="primaryInfo"
-                                ref={
-                                    element => (
-                                        this.primaryInfoEl = element
-                                    )
-                                }
-                            >
+                            <div className="primaryInfo">
                                 <ContentList
                                     className="featured"
-                                    items={data.data.externalResources}
+                                    items={data.externalResources}
                                 />
                             </div>
                         </React.Fragment>)
@@ -215,13 +215,16 @@ class ResultsListPage extends ResultsPage<Props, ExtraState> {
     }
 
     renderKeyInfo() {
+        if (!this.category) {
+            return null
+        }
         return (
             <Query
                 query={externalResourcesQuery}
                 loadingComponent={this.renderLoadingComponent()}
                 args={
                     {
-                        "category": [this.category.title],
+                        "category": [this.CMSCategoryName],
                         "tag": [
                             "Information",
                         ],
@@ -230,21 +233,22 @@ class ResultsListPage extends ResultsPage<Props, ExtraState> {
                         ],
                     }
                 }
+                dataLoadedCallback={data =>
+                    data.externalResources.length > 0 &&
+                        this.setState({ hasInfo: true })
+                }
             >
-                {data => (
-                    !data.data.externalResources.length > 0 ? "" : (
+                {({data}) => (
+                    !data.externalResources.length > 0 ? "" : (
                         <React.Fragment>
                             <a
                                 className="anchor"
                                 id="information"
                             />
-                            <div
-                                className="keyInfo"
-                                ref={element => (this.keyInfoEl = element)}
-                            >
+                            <div className="keyInfo">
                                 <h3>Key information</h3>
                                 <ContentList
-                                    items={data.data.externalResources}
+                                    items={data.externalResources}
                                 />
                             </div>
                         </React.Fragment>
--- src/queries/query.js
+++ src/queries/query.js
@@ -5,50 +5,61 @@ import { useQuery } from "@apollo/react-hooks";
 // $FlowIgnore
 import type { DocumentNode } from "graphql";
 
-const Query = ({ children, query, args, loadingComponent, errorComponent }: {
-  children: any,
-  query: DocumentNode,
-  args?: Object,
-  loadingComponent?: any,
-  errorComponent?: any
-}) => {
+const Query = (
+    {
+        children,
+        query,
+        args,
+        loadingComponent,
+        errorComponent,
+        dataLoadedCallback,
+    }: {
+        children: any,
+        query: DocumentNode,
+        args?: Object,
+        loadingComponent?: any,
+        errorComponent?: any,
+        dataLoadedCallback?: function,
+    }
+) => {
     /*
-      Paramaters:
-          query: A gql query string.
-          args GQL arguments to be used in the query.
+    Parameters:
+        query: A gql query string.
+        args GQL arguments to be used in the query.
 
-      This is a simple query component which takes a query and an
-      a unique identifier and performs a graphql query for the record.
+    This is a simple query component which takes a query and an
+    a unique identifier and performs a graphql query for the record.
 
-      The component will return a data object with the resulting record.
+    The component will return a data object with the resulting record.
 
-      To use this component first declare a query such as:
+    To use this component first declare a query such as:
 
-      import gql from "graphql-tag";
+    import gql from "graphql-tag";
 
-      const generateQuery = gql`
+    const generateQuery = gql`
         query Content($id: ID!) {
-          content(id: $id) {
-            id
-            Description
-            Body
-          }
+            content(id: $id) {
+                id
+                Description
+                Body
+            }
         }
-      `;
+    `;
 
-      then import this module and call it as:
+    then import this module and call it as:
 
-      <Query query={pageQuery}
-             id={1}
-      >
-          {({data}) => {
-              return <p>{data.content.Body}</p>;
-          }}
-      </Query>
+    <Query query={pageQuery}
+        id={1}
+    >
+        {({data}) => {
+            return <p>{data.content.Body}</p>;
+        }}
+    </Query>
     */
 
     const { data, loading, error } = useQuery(query, {
         variables: args,
+        onCompleted: dataLoadedCallback,
     });
 
     if (loading) {
