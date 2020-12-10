5cbdc0f909423fa275dc7e1ca2bab74e26c735e3
Michael Adams <michaela@infoxchange.org>
2020-12-11 10:11:32 +1100

    Only implement a banner if banner key provided.

--- src/pages/DynamicPage.js
+++ src/pages/DynamicPage.js
@@ -67,7 +67,7 @@ class DynamicPage extends React.Component<Props, void> {
                         return (
                             <StaticPage
                                 title={page.Title}
-                                bannerName={page.Banner.Key}
+                                bannerName={page.Banner ? page.Banner.Key : ""}
                                 bannerPrimary={page.BannerTextPrimary}
                                 bannerSecondary={page.BannerTextSecondary}
                             >
