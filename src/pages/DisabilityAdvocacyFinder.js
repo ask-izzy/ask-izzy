6b58282fa96f64f92f9d34e78d4aa5fc339caf37
Callum Gare <cgare@infoxchange.org>
2021-01-12 13:55:54 +1100

    Modify design of DisabilityAdvocacyFinder page

--- src/pages/DisabilityAdvocacyFinder.js
+++ src/pages/DisabilityAdvocacyFinder.js
@@ -1,5 +1,4 @@
 /* @flow */
-/* eslint-disable max-len */
 
 import React from "react";
 import { Link } from "react-router-dom";
@@ -7,60 +6,63 @@ import StaticPage from "./StaticPage";
 import FlatButton from "../components/FlatButton";
 
 class DisabilityAdvocacyFinder extends React.Component<{}, void> {
-
     render() {
-
         return (
             <StaticPage
-                title="Disability advocacy"
+                title="Disability Advocacy"
                 bannerPrimary="Disability Advocacy"
                 bannerSecondary="Find advocacy services near you"
                 bannerName="advocacy static"
             >
                 <div className="DisabilityAdvocacyFinder">
-                    <h2>Ask Izzy - Disability Advocacy Finder</h2>
+                    <h2>Using Ask Izzy to find a Disability Advocate</h2>
                     <p>
-                        From this page you can search for Disability Advocacy
-                        providers in your area. These services are intended to
-                        provide people with disability with access to
-                        independent advocacy that promotes, protects and ensures
-                        their full and equal enjoyment of all human rights.
+                        People with disability who need someone to speak up for
+                        them can use Ask Izzy to search for independent
+                        Disability Advocacy providers in their area. These
+                        services provide access to professionals who can ensure
+                        the choices and rights of people with disability are
+                        respected and they are being treated fairly.
                     </p>
                     <Link
                         to="/search/Disability Advocacy Providers"
                         className="findAdvocacyButton"
                     >
                         <FlatButton
-                            label="Find advocacy"
+                            label="Find an advocate on Ask Izzy"
                             onClick={() => {}}
                         />
                     </Link>
                     <p>
-                        <strong>For more information about disability advocacy</strong>
-                        <br />
+                        <h3>
+                            Want more information about disability advocacy?
+                        </h3>
                         <a
-                            href="https://disabilityadvocacyfinder.dss.gov.au/disability/ndap/about"
-                            target="_blank"
+                            href={"https://disabilityadvocacyfinder.dss.gov." +
+                                "au/disability/ndap/about"}
                         >
                             See the Department of Social Services website
                         </a>
                     </p>
-                    <h3>Other Ask Izzy Services</h3>
-                    <p>
-                        Ask Izzy is a website that connects people in need with
-                        housing, a meal, money help, family violence support,
-                        counselling and much more.
-                    </p>
-                    <p>
-                        It is free and anonymous, with over 370,000 services listed across Australia.
-                    </p>
-                    <p>
-                        <Link
-                            to="/"
-                        >
-                            See what support Ask Izzy can offer.
-                        </Link>
-                    </p>
+                    <div className="askIzzyInfoBox">
+                        <h2>Other support services on Ask Izzy</h2>
+                        <p>
+                            Ask Izzy is a website that connects people in need
+                            with housing, a meal, money help, family violence
+                            support, counselling and much more.
+                        </p>
+                        <p>
+                            It is free and anonymous, with over 370,000 services
+                            listed across Australia.
+                        </p>
+                        <p>
+                            <Link
+                                to="/"
+                            >
+                                Search Ask Izzy for support now.
+                            </Link>
+                        </p>
+                    </div>
                 </div>
             </StaticPage>
         );
