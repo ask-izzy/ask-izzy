abf24c4fb81f05f33e463c2db0cae309818c66db
trentmarino <tmarino@infoxchange.org>
2021-04-14 17:44:47 +1000

    Just a bit of a play with Ask izzy  - Separated out the QuickExit to its own component  - split the covid19 static page into separate components to reduce duplicate code. with the content being loaded from a services including certain helper components (the components could ideally be separated out into individual files, or as part of a component utils file)

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -4,6 +4,7 @@ import React from "react";
 
 import components from "../components";
 import icons from "../icons";
+import QuickExit from "./QuickExit";
 
 type Props = {
     title: string,
@@ -11,18 +12,22 @@ type Props = {
     backMessage?: string,
 }
 
+
 class AppBar extends React.Component<Props, void> {
     static sampleProps = {
         default: {
             title: "App bar",
-            onBackTouchTap: function() {},
+            onBackTouchTap: function() {
+            },
         },
     };
 
 
+
+
     render() {
-        let redirectUri = "http://www.bom.gov.au/";
-        let tooltip = "To leave this website quickly, click the 'Quick" +
+        const redirectUri = "http://www.bom.gov.au/";
+        const tooltip = "To leave this website quickly, click the 'Quick" +
             " Exit' button. If you are in immediate danger call 000 " +
             "(Australian emergency line), for advice about family " +
             "violence call 1800 Respect on 1800 737 732 (Helpline).";
@@ -32,23 +37,13 @@ class AppBar extends React.Component<Props, void> {
                 <div className="AppBar">
                     {this.renderBackButton()}
                     <h1 className="title">{this.props.title}</h1>
-                    <div className="button-container">
-                        <a className="quick-exit"
-                            href={redirectUri}
-                            title={tooltip}
-                        >
-                            <div className="quick-exit">
-                                <span className="longer-text">
-                                    Quick Exit ⨉
-                                </span>
-                                <span className="shorter-text">
-                                    Exit ⨉
-                                </span>
-                            </div>
-                        </a>
-                    </div>
+                    <QuickExit
+                        className="button-container"
+                        redirectUri={redirectUri}
+                        tooltip={tooltip}
+                    />
                 </div>
-                <div className="AppBarSpacer" />
+                <div className="AppBarSpacer"/>
             </div>
         );
     }
@@ -59,7 +54,7 @@ class AppBar extends React.Component<Props, void> {
                 className="BackButton button-container"
                 onClick={this.props.onBackTouchTap}
             >
-                <icons.ChevronBack />
+                <icons.ChevronBack/>
                 <span className="back-label">
                     {this.props.backMessage}
                 </span>
---
+++ src/pages/Covid19/Covid19.js
@@ -0,0 +1,94 @@
+import React, {useEffect, useState} from "react";
+import StaticPage from "../StaticPage";
+import Covid19Card from "./Covid19Card";
+import Phone from "../../components/Phone";
+import "./Covid19.scss";
+import {Link} from "react-router-dom";
+import {states} from "./Covid19Service";
+import {windowWidth} from "../../components/effects/WindowWidth";
+import sendEvent from "../../google-tag-manager";
+function Covid19() {
+
+    const [mobileView, setMobileView] = useState(false);
+    const width = windowWidth();
+
+    useEffect(() => {
+        setMobileView(width <= 768);
+    }, [width])
+
+    useEffect(() => {
+        sendEvent({
+            event: "categoryPageLoad",
+            categoryName: "Covid-19",
+            isTopical: true,
+        });
+        sendEvent({
+            categoryName: undefined,
+            isTopical: undefined,
+        });
+    }, [])
+
+    return (
+
+        <StaticPage
+            title=""
+            bannerName="money-help static"
+            className="Covid19Page"
+            bannerPrimary="Coronavirus (COVID-19) information and services"
+            bannerSecondary=""
+        >
+            <div className="nationalHotline">
+                <h2>COVID-19 National Hotline</h2>
+                The Coronavirus Health Information Line provides general
+                information relating to coronavirus (COVID-19) for the
+                general community. The line operates 24 hours a day, seven
+                days a week.
+                <Phone
+                    number="1800 020 080"
+                    comment=""
+                    kind=""
+                />
+            </div>
+
+            <h2>Federal Government Information</h2>
+            <p>
+                For information from the federal government please visit
+                the{" "}
+                <a
+                    href="https://www.health.gov.au/news/health-alerts/novel-coronavirus-2019-ncov-health-alert"
+                    rel="noopener noreferer"
+                    target="_blank"
+                >Australian Department of Health website</a> for the latest
+                medical advice, news and information.
+            </p>
+
+            <div className="separator"/>
+            <h2>State Government Information</h2>
+            <hr/>
+            {states.map(state =>
+                <div key={state.title}>
+                    <Covid19Card
+                        mobileView={mobileView}
+                        {...state}
+                    />
+                    <hr/>
+                </div>)}
+            <hr/>
+            <div className="izzyCanHelp">
+                <h2>Ask Izzy can help</h2>
+                <p>
+                    You can use Ask Izzy to{" "}
+                    <Link to="/search/coronavirus/personalise">
+                        find nearby clinics
+                    </Link> providing services and testing specific to
+                    coronavirus.
+                </p>
+            </div>
+        </StaticPage>
+
+    )
+}
+
+
+
+export default Covid19
