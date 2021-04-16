eca949d6777e30875c5c2a9f9612ebd66250dd4a
trentmarino <tmarino@infoxchange.org>
2021-04-16 10:52:48 +1000

    Resolved PR comments
     - added flow type checking to the QuickExit.js including default props
     - Reverted some formatting issues

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -17,31 +17,18 @@ class AppBar extends React.Component<Props, void> {
     static sampleProps = {
         default: {
             title: "App bar",
-            onBackTouchTap: function() {
-            },
+            onBackTouchTap: function() {},
         },
     };
 
 
-
-
     render() {
-        const redirectUri = "http://www.bom.gov.au/";
-        const tooltip = "To leave this website quickly, click the 'Quick" +
-            " Exit' button. If you are in immediate danger call 000 " +
-            "(Australian emergency line), for advice about family " +
-            "violence call 1800 Respect on 1800 737 732 (Helpline).";
-
         return (
             <div className="AppBarContainer">
                 <div className="AppBar">
                     {this.renderBackButton()}
                     <h1 className="title">{this.props.title}</h1>
-                    <QuickExit
-                        className="button-container"
-                        redirectUri={redirectUri}
-                        tooltip={tooltip}
-                    />
+                    <QuickExit />
                 </div>
                 <div className="AppBarSpacer"/>
             </div>
@@ -54,7 +41,7 @@ class AppBar extends React.Component<Props, void> {
                 className="BackButton button-container"
                 onClick={this.props.onBackTouchTap}
             >
-                <icons.ChevronBack/>
+                <icons.ChevronBack />
                 <span className="back-label">
                     {this.props.backMessage}
                 </span>
