f4569aa880730c757449a12dd312b6de9191f74e
trentmarino <tmarino@infoxchange.org>
2021-04-21 12:13:15 +1000

    Add search personalisation pages to static page list :)
    New issues have been added to the existing-accessibility-issues file, these issues already existed
    but wern't recognised until generated those static files.

--- src/contexts/router-context.js
+++ src/contexts/router-context.js
@@ -1,7 +1,7 @@
 /* @flow */
 import React from "react";
 import { withRouter } from "react-router";
-
+import storage from "../storage";
 const RouterContext = React.createContext<Object>();
 
 export default RouterContext
@@ -15,6 +15,13 @@ export const InjectRouterContext = withRouter(
                 match: matchedRoute,
             },
         }
+
+        // if a search value is in the URL but not stored, set the search
+        // text in storage.
+        if (!storage.getSearch() && matchedRoute.params.search) {
+            storage.setSearch(matchedRoute.params.search)
+        }
+
         return (
             <RouterContext.Provider value={context}>
                 {children}
--- src/pages/personalisation/FreeTextAreYouSafe.js
+++ src/pages/personalisation/FreeTextAreYouSafe.js
@@ -5,6 +5,11 @@ import storage from "../../storage";
 import ViolenceKeywords from "../../constants/free-text-keywords";
 
 export default class FreeTextAreYouSafe extends AreYouSafe {
+
+    static staticShowPage(): boolean {
+        return true;
+    }
+
     static showPage(): boolean {
         const searchString = storage.getSearch();
 
