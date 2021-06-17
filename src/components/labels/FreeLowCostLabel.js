a63a05bac80167ad169a615374a89196f152d81e
trentmarino <tmarino@infoxchange.org>
2021-06-17 10:22:17 +1000

    Add a tooltip to the free/low cost label

--- src/components/labels/FreeLowCostLabel.js
+++ src/components/labels/FreeLowCostLabel.js
@@ -2,9 +2,27 @@
 import * as React from "react";
 
 function FreeLowCostLabel(): React.Node {
+    const [showDescription, setShowDescription] = React.useState(false);
+    const labelText = "Free / Low Cost"
+    const labelDescription = "You may be required to present " +
+        "a Medicare or concession card"
     return (
-        <div className="FreeLowCostLabel">
-            Free / Low Cost
+        <div
+            className="FreeLowCostLabel"
+            onClick={evt => {
+                evt.stopPropagation();
+                setShowDescription(!showDescription)
+            }}
+            onMouseLeave={() => setShowDescription(false)}
+        >
+            {showDescription && (
+                <div className="labelDesc">
+                    <div className="labelDescContent">
+                        {labelDescription}
+                    </div>
+                </div>
+            )}
+            {labelText}
         </div>
     )
 }
