158b4d0fe2d7898751ca012ce7d334daab87b07e
trentmarino <tmarino@infoxchange.org>
2021-08-11 14:04:37 +1000

    Add back to disability gateway button to the Advocacy finder page
    - Added a "Go back to the disability gateway" button to the bottom of the Disability Advocacy page
    - made the page more accessible  for screen readers

--- src/pages/DisabilityAdvocacyFinder.js
+++ src/pages/DisabilityAdvocacyFinder.js
@@ -1,74 +1,88 @@
 /* @flow */
 
 import type {Node as ReactNode} from "React";
-import React from "react";
+import React, {useContext} from "react";
 
 import Link from "../components/Link";
 import StaticPage from "./StaticPage";
 import FlatButton from "../components/FlatButton";
+import icons from "../icons";
+import routerContext from "../contexts/router-context";
 
-class DisabilityAdvocacyFinder extends React.Component<{}, void> {
-    render(): ReactNode {
-        return (
-            <StaticPage
-                title="Disability Advocacy"
-                bannerPrimary="Disability Advocacy"
-                bannerSecondary="Find advocacy services near you"
-                bannerName="advocacy static"
-            >
-                <div className="DisabilityAdvocacyFinder">
-                    <h2>Using Ask Izzy to find a Disability Advocate</h2>
-                    <p>
-                        People with disability who need someone to speak up for
-                        them can use Ask Izzy to search for independent
-                        Disability Advocacy providers in their area. These
-                        services provide access to professionals who can ensure
-                        the choices and rights of people with disability are
-                        respected and they are being treated fairly.
-                    </p>
+function DisabilityAdvocacyFinder(): ReactNode {
+
+    const { router } = useContext(routerContext)
+
+    const advocacyProviders = "/search/Disability Advocacy Providers";
+    const disabilityGatewayURL = "https://www.disabilitygateway.gov.au/";
+    const disabilityAdvocacyURL = "https://disabilityadvocacyfinder.dss.gov." +
+                            "au/disability/ndap/about"
+
+    return (
+        <StaticPage
+            title="Disability Advocacy"
+            bannerPrimary="Disability Advocacy"
+            bannerSecondary="Find advocacy services near you"
+            bannerName="advocacy static"
+        >
+            <div className="DisabilityAdvocacyFinder">
+                <h2>Using Ask Izzy to find a Disability Advocate</h2>
+                <p>
+                    People with disability who need someone to speak up for
+                    them can use Ask Izzy to search for independent
+                    Disability Advocacy providers in their area. These
+                    services provide access to professionals who can ensure
+                    the choices and rights of people with disability are
+                    respected and they are being treated fairly.
+                </p>
+                <FlatButton
+                    label="Find an advocate on Ask Izzy"
+                    onClick={() => {
+                        router.navigate(advocacyProviders)
+                    }}
+                />
+                <div>
+                    <h3>
+                        Want more information about disability advocacy?
+                    </h3>
                     <Link
-                        to="/search/Disability Advocacy Providers"
-                        className="findAdvocacyButton"
+                        to={disabilityAdvocacyURL}
                     >
-                        <FlatButton
-                            label="Find an advocate on Ask Izzy"
-                            onClick={() => {}}
-                        />
+                        See the Department of Social Services website
                     </Link>
-                    <div>
-                        <h3>
-                            Want more information about disability advocacy?
-                        </h3>
-                        <Link
-                            to={"https://disabilityadvocacyfinder.dss.gov." +
-                                "au/disability/ndap/about"}
-                        >
-                            See the Department of Social Services website
-                        </Link>
-                    </div>
-                    <div className="askIzzyInfoBox">
-                        <h2>Other support services on Ask Izzy</h2>
-                        <p>
-                            Ask Izzy is a website that connects people in need
-                            with housing, a meal, money help, family violence
-                            support, counselling and much more.
-                        </p>
-                        <p>
-                            It is free and anonymous, with over 370,000 services
-                            listed across Australia.
-                        </p>
-                        <p>
-                            <Link
-                                to="/"
-                            >
-                                Search Ask Izzy for support now.
-                            </Link>
-                        </p>
-                    </div>
                 </div>
-            </StaticPage>
-        );
-    }
+                <div className="askIzzyInfoBox">
+                    <h2>Other support services on Ask Izzy</h2>
+                    <p>
+                        Ask Izzy is a website that connects people in need
+                        with housing, a meal, money help, family violence
+                        support, counselling and much more.
+                    </p>
+                    <p>
+                        It is free and anonymous, with over 370,000 services
+                        listed across Australia.
+                    </p>
+                    <p>
+                        <Link
+                            to="/"
+                        >
+                            Search Ask Izzy for support now.
+                        </Link>
+                    </p>
+                </div>
+                <div className="backToDSS">
+                    <FlatButton
+                        onClick={() => {
+                            router.navigate(disabilityGatewayURL)
+                        }}
+                    >
+                        <icons.ChevronBack/>{" "}
+                        Go back to the disability gateway
+                    </FlatButton>
+                </div>
+            </div>
+        </StaticPage>
+    );
 }
 
 export default DisabilityAdvocacyFinder;
