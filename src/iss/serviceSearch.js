c99f9b789e60137fd17c80b7d5600a4dcce5ce19
Callum Gare <cgare@infoxchange.org>
2022-02-15 17:32:24 +1100

    draft

--- src/iss/serviceSearch.js
+++ src/iss/serviceSearch.js
@@ -1,205 +1,114 @@
 /* @flow */
-import lru from "lru-cache";
-
-import Service, {attachTransportTimes} from "./Service";
+import Service from "../services/Service";
 import storage from "../storage";
-import ServiceSearchCache from "./ServiceSearchCache";
-import {searchIss} from "./search";
-import type {searchResultsMeta} from "./search";
-import {serialiseUrlQueryParams} from "../utils/url"
-import {
-    TryWithDefault,
-} from "../timeout";
-import * as gtm from "../google-tag-manager";
+import {getIssClient, getIss3Client} from "./client"
 import type { RouterContextObject } from "../contexts/router-context";
-import {getCategoryFromRouter} from "../utils/personalisation"
-import WhoIsLookingForHelpPage from
-    "../pages/personalisation/WhoIsLookingForHelp"
+import type {
+    SearchQuery as IssSearchQuery,
+    ISS3SearchQuery as Iss3SearchQuery,
+} from "../ix-web-js-client/apis/iss.js"
+import type {SearchQuery as IzzySearchQuery} from "./searchQueryBuilder"
 
-export type serviceSearchRequest = {|
-    q?: string,
-    service_type?: Array<string>,
-    service_type_raw?: Array<string>,
-    site_id?: number,
-    name?: string,
+export const previousSearchQueryStorageKey = "previousSearchQuery"
 
-    minimum_should_match?: string,
-
-    area?: string,
-    location?: string,
-    type?: string,
-    age_group?: Array<string>,
-    client_gender?: Array<string>,
-
-    catchment?: "prefer"|"true"|"false",
-    is_bulk_billing?: boolean,
-    show_in_askizzy_health?: boolean,
-
-    limit?: number,
-    key?: string,
-|};
-
-export type serviceSearchResultsMeta = {
-    ...searchResultsMeta,
-    available_count: number,
-    location: {
-        name: string,
-        suburb: string,
-        state: string,
-        lat: number,
-        lon: number,
-    },
+export function createServiceSearch(query: IzzySearchQuery): PaginatedSearch {
+    return new PaginatedSearch(query)
 }
 
-export type serviceSearchResults = {|
-    meta: serviceSearchResultsMeta,
-    services: Array<Service>,
-|};
-
-/**
- * Execute a search against ISS.
- *
- * @param {searchRequest} query - either a query string, or an object of
- * search parameters.
- *
- * @returns {Promise<serviceSearchResults>} search results from ISS.
- */
-export async function initialSearchForServices(
-    query: serviceSearchRequest,
-): Promise<serviceSearchResults> {
-    let request_: serviceSearchRequest = {
-        q: "",
-        type: "service",
-        limit: 10,
-    };
-
-    const searchUrlPath = "/api/v3/search/";
-    const previousSearchUrl: string =
-        (storage.getItem("previous_search_url"): any);
-
-    Object.assign(request_, query);
-    let searchUrl = serialiseUrlQueryParams(searchUrlPath, request_);
-
-    const whoIsLookingForHelp = String(storage.getItem(
-        WhoIsLookingForHelpPage.defaultProps.name
-    ))
-    if (searchUrl != previousSearchUrl) {
-        gtm.emit({
-            event: "New Search On Behalf Of",
-            eventCat: "Services Searched",
-            eventAction: "Help Seeker Type",
-            eventLabel: whoIsLookingForHelp,
-            sendDirectlyToGA: true,
-        });
-
-        gtm.emit({
-            event: "Action Triggered - New Search",
-            eventCat: "Action triggered",
-            eventAction: "New search request",
-            eventLabel: whoIsLookingForHelp ?
-                whoIsLookingForHelp
-                : "<not answered>",
-            sendDirectlyToGA: true,
-        });
+type QueryInfo = (
+    {
+        izzyQuery: IzzySearchQuery,
+        issQuery: IssSearchQuery,
+        issVersion: "4",
+    } | {
+        izzyQuery: IzzySearchQuery,
+        issQuery: Iss3SearchQuery,
+        issVersion: "3",
     }
-    storage.setItem("previous_search_url", searchUrl);
-    return await searchForServices(searchUrlPath, request_);
-}
+)
 
-export async function searchForServices(
-    path: string,
-    data: ?serviceSearchRequest,
-): Promise<serviceSearchResults> {
-    const url_ = serialiseUrlQueryParams(path, data);
+export class PaginatedSearch {
+    #queryInfo: QueryInfo;
+    #pagesLoaded: number = 0;
+    #maxPageSize: number;
+    #loadedServices: Array<Service> = [];
+    #cake: IzzySearchQuery;
 
-    if (searchResultsCache.getPageForQuery(url_)) {
-        const resultWithAllPages = searchResultsCache
-            .getAllPagesForQuery(url_)
-        if (!resultWithAllPages) {
-            // This should never happen. If we've cached this specific page then
-            // we should have a cache match for all pages relating to this query
-            console.warn("Search cache hit for single page but not all pages")
-        } else {
-            return resultWithAllPages;
+    constructor(query: IzzySearchQuery) {
+        const {maxPageSize = 10, apiVersion = "3", ...remainingQuery} = query
+        this.#maxPageSize = maxPageSize
+        if (apiVersion === "4") {
+            this.#queryInfo = {
+                izzyQuery: query,
+                issQuery: convertIzzySearchQueryToIss(remainingQuery),
+                issVersion: "4",
+            }
+        } else if (apiVersion === "3") {
+            let issQuery
+            if (
+                storage.getDebug() &&
+                storage.getJSON("issParamsOverride")
+            ) {
+                issQuery = storage.getJSON("issParamsOverride")
+            } else {
+                issQuery = convertIzzySearchQueryToIss3(remainingQuery)
+            }
+            this.#queryInfo = {
+                izzyQuery: query,
+                issQuery,
+                issVersion: "3",
+            }
         }
     }
 
-    const {meta, objects} = await await searchIss(path, data);
+    async loadNextPage() {
+        const queryInfo = this.#queryInfo
+        let services: Service[]
 
-    // convert objects to ISS search results
-    let services: Array<Service> = objects.map(
-        (object: Object): Service => new Service(object)
-    );
+        if (queryInfo.issVersion === "4") {
+            const issClient = await getIssClient()
+            const res = await issClient.search({
+                ...queryInfo.issQuery,
+                page: {
+                    current: this.#pagesLoaded + 1,
+                    size: this.#maxPageSize,
+                },
+            })
+            this.#pagesLoaded++;
+            services = res.results.map(
+                serviceData => new Service(serviceData)
+            )
+        } else if (queryInfo.issVersion === "3") {
+            const iss3Client = await getIss3Client()
+            const res = await iss3Client.search({
+                ...queryInfo.issQuery,
+                offset: this.#pagesLoaded * this.#maxPageSize,
+                limit: this.#maxPageSize,
+            })
+            services = res?.objects.map(
+                serviceData => new Service(serviceData)
+            ) || []
+        } else {
+            throw Error(
+                `Api version "${queryInfo.issVersion}" is not recognised`
+            )
+        }
 
-    if (storage.getUserGeolocation()) {
-        services = await TryWithDefault(
-            3000,
-            attachTransportTimes(services),
-            services
-        )
+        this.#loadedServices.push(...services)
+        this.#pagesLoaded++;
     }
 
-    services.forEach((service) =>
-        serviceCache.set(service.id, service)
-    )
-
-    const searchResults = {
-        meta,
-        services,
+    get loadedServices(): Array<Service> {
+        return this.#loadedServices
     }
 
-    searchResultsCache.revise(url_, searchResults);
-
-    // searchResults should never be returned but we provided it as a fail safe
-    // to keep flow happy.
-    return searchResultsCache.getAllPagesForQuery(url_) || searchResults;
-}
-
-const searchResultsCache = new ServiceSearchCache();
-const serviceCache = lru({
-    max: 150, // Only track 150 keys in the cache
-    maxAge: 1000 * 60 * 60, // Discard anything over 1 hour
-});
-
-export function getServiceFromCache(serviceId: number): ?Service {
-    return serviceCache.get(serviceId)
-}
-
-export function forEachServiceFromCache(
-    callback: (service: Service) => any
-): void {
-    serviceCache.forEach(callback)
-}
-
-export function getInitialSearchRequest(
-    router: $PropertyType<RouterContextObject, 'router'>
-): serviceSearchRequest {
-    let request: serviceSearchRequest
-    const category = getCategoryFromRouter(router)
-
-    if (category) {
-        request = {...category.search};
-    } else if (router.match.params.search) {
-        request = {
-            q: decodeURIComponent(router.match.params.search),
-        };
-    } else {
-        request = {
-            q: "undefined-search",
-        };
+    get numOfPagesLoaded(): number {
+        return this.#pagesLoaded
     }
 
-    // A special case for the "Find advocacy" button on the
-    // DisabilityAdvocacyFinder page.
-    if (request.q === "Disability Advocacy Providers") {
-        // $FlowIgnore because flow is acting wack about "decodeURIComponent"
-        // above for some reason
-        // $FlowIgnore
-        request.service_type_raw = ["disability advocacy"]
-        request.q = "disability"
+    get issQuery(): IssSearchQuery | Iss3SearchQuery {
+        return this.#queryInfo.issQuery
     }
-
-    return request
 }
 
 export function isDisabilityAdvocacySearch(
@@ -207,4 +116,179 @@ export function isDisabilityAdvocacySearch(
 ): boolean {
     return decodeURIComponent(router.match.params.search) ===
         "Disability Advocacy Providers"
+}
+
+export function convertIzzySearchQueryToIss(
+    query: IzzySearchQuery
+): IssSearchQuery {
+    const issQuery: IssSearchQuery = {
+        filters: {
+            all: [
+                {
+                    object_type: "Service",
+                },
+            ],
+        },
+        boosts: {
+            is_crisis: {
+                type: "value",
+                value: "true",
+                operation: "multiply",
+                factor: 1.5,
+            },
+        },
+    }
+
+    if (query.term) {
+        issQuery.query = query.term.join(" ")
+    }
+
+    if (query.serviceTypes) {
+        issQuery.filters?.all?.push({
+            service_types: query.serviceTypes,
+        })
+    }
+
+    if (query.clientGenders) {
+        const genderBoost = query.clientGenders.map(
+            gender => ({
+                factor: 1.5,
+                operation: "multiply",
+                type: "value",
+                value: gender,
+            })
+        )
+        if (!issQuery.boosts) {
+            issQuery.boosts = {}
+        }
+        issQuery.boosts.target_gender = genderBoost
+    }
+
+    if (query.location) {
+        const location = query.location
+        const state = location.name.match(/, (\w+)$/)?.[1]
+
+        if (!issQuery.boosts) {
+            issQuery.boosts = {}
+        }
+        issQuery.boosts.location_state = {
+            type: "value",
+            value: state,
+            operation: "multiply",
+            factor: 1.5,
+        }
+        const coordinates = location.coordinates
+        if (coordinates) {
+            issQuery.boosts.location_approximate_geopoint = [
+                {
+                    type: "proximity",
+                    function: "linear",
+                    center: `${coordinates.latitude},${coordinates.longitude}`,
+                    factor: 7,
+                },
+            ]
+
+            issQuery.filters?.all?.push(
+                {
+                    location_approximate_geopoint: {
+                        center:
+                            `${coordinates.latitude},${coordinates.longitude}`,
+                        distance: 300,
+                        unit: "km",
+                    },
+                }
+            )
+        }
+
+
+        // issQuery.boosts.location_postcode = {
+        //     type: "value",
+        //     value: [
+        //         "6009",
+        //         "6007",
+        //         "6151",
+        //         "6008",
+        //         "6006",
+        //         "6000",
+        //         "6004",
+        //         "6050",
+        //         "6003",
+        //         "6005"
+        //     ],
+        //     "operation": "multiply",
+        //     "factor": 2
+        // }
+    }
+    console.log("converting izzy to iss", query, issQuery)
+
+    return issQuery
+}
+
+
+export function convertIzzySearchQueryToIss3(
+    query: IzzySearchQuery
+): Iss3SearchQuery {
+    // age_group: string,
+    // area: string,
+    // catchment: string,
+    // client_gender: string,
+    // key: string,
+    // limit: number,
+    // location: string,
+    // minimum_should_match: string,
+    // q: string,
+    // service_type: string,
+    // type: string,
+    // site_id?: number,
+    // show_in_askizzy_health?: boolean,
+    // is_bulk_billing?: boolean
+    const issQuery: $Shape<Iss3SearchQuery> = {
+    }
+
+    if (query.term) {
+        issQuery.q = query.term.join(" ")
+    }
+
+    if (query.siteId) {
+        issQuery.site_id = query.siteId
+    }
+
+    if (query.serviceTypes) {
+        issQuery.service_type = query.serviceTypes.map(
+            gender => gender.toLowerCase()
+        )
+    }
+
+    if (query.clientGenders) {
+        issQuery.client_gender = query.clientGenders.map(
+            gender => ({
+                Female: "f",
+                Male: "m",
+                Diverse: "x",
+                unspecified: "u",
+            }[gender])
+        )
+    }
+
+    if (query.ageGroups) {
+        issQuery.age_group = query.ageGroups.map(
+            gender => gender.toLowerCase()
+        )
+
+    }
+
+    if (query.catchment) {
+        issQuery.catchment = query.catchment
+    }
+
+    if (query.location) {
+        issQuery.area = query.location.name
+        if (query.location.coordinates) {
+            issQuery.location = `${query.location.coordinates.longitude}E` +
+                `${query.location.coordinates.latitude}N`
+        }
+    }
+    console.log("converting izzy to iss 3", query, issQuery)
+
+    return issQuery
 }
