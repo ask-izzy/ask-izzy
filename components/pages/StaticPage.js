4ff0a00618501af10bbaa477d60acb0f358059f3
ggarcia <ggarcia@infoxchange.org>
2022-10-06 10:12:58 +1100

    refactor StaticPage component

--- components/pages/StaticPage.js
+++ components/pages/StaticPage.js
@@ -2,44 +2,46 @@
 /* eslint-disable max-len */
 
 import * as React from "react";
+import type {Node as ReactNode} from "React";
 import HeaderBar from "@/src/components/HeaderBar";
 import BrandedFooter from "@/src/components/BrandedFooter";
 
 type Props = {
   title: string,
   bannerName: string,
-  children: React.Node,
-  className: string,
+  children?: React.Node,
+  className?: string,
   bannerPrimary?: string,
   bannerSecondary?: string,
 }
 
-export default class StaticPage extends React.Component<Props> {
-    static defaultProps: {
-        children: null,
-        className: "",
-    };
-
-    render(): React.Element<"div"> {
-
-        return (
-            <div className={`StaticPage ${this.props.className || ""}`}>
-                <div>
-                    <HeaderBar
-                        className="prominentHeading"
-                        primaryText={this.props.bannerPrimary || this.props.title}
-                        secondaryText={this.props.bannerSecondary || null}
-                        bannerName={this.props.bannerName || "homepage"}
-                    />
-                </div>
-                <main>
-                    <div className="body">
-                        {this.props.children}
-                    </div>
-                    <br />
-                </main>
-                <BrandedFooter />
+function StaticPage({
+    title,
+    bannerName,
+    children = null,
+    className = "",
+    bannerPrimary,
+    bannerSecondary,
+}: Props): ReactNode {
+    return (
+        <div className={`StaticPage ${className || ""}`}>
+            <div>
+                <HeaderBar
+                    className="prominentHeading"
+                    primaryText={bannerPrimary || title}
+                    secondaryText={bannerSecondary || null}
+                    bannerName={bannerName || "homepage"}
+                />
             </div>
-        );
-    }
+            <main>
+                <div className="body">
+                    {children}
+                </div>
+                <br />
+            </main>
+            <BrandedFooter />
+        </div>
+    )
 }
+
+export default StaticPage
