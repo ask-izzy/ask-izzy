898a4fafad70603c84006a6bc92aa2b046ea546b
ggarcia <ggarcia@infoxchange.org>
2022-12-07 09:38:22 +1100

    convert not found page to typescript

--- components/pages/NotFoundStaticPage.tsx
+++ components/pages/NotFoundStaticPage.tsx
@@ -1,15 +1,13 @@
-/* @flow */
 /* eslint-disable max-len */
 
 import React from "react";
-import type {Node as ReactNode} from "react";
 import Head from "next/head"
 
 import HeaderBar from "@/src/components/HeaderBar";
 import BrandedFooter from "@/src/components/BrandedFooter";
 import Link from "@/src/components/base/Link";
 
-function NotFoundStaticPage(): ReactNode {
+function NotFoundStaticPage() {
     return (
         <div
             className="StaticPage"
@@ -28,7 +26,7 @@ function NotFoundStaticPage(): ReactNode {
             />
             <div className="body">
                 <p>Sorry, but it looks as though the page you are trying to find does not exist. It might be because you followed an old link, or typed the address incorrectly—but it was most likely our fault for moving something.</p>
-                <p>Please contact support at 03 9418 7466 or <Link to={`mailto:${process.env.NEXT_PUBLIC_SITE_EMAIL}`}>{process.env.NEXT_PUBLIC_SITE_EMAIL}</Link>, and we'll try to resolve the problem as soon as we can.</p>
+                <p>Please contact support at 03 9418 7466 or <Link to={`mailto:${process.env.NEXT_PUBLIC_SITE_EMAIL}`}>{process.env.NEXT_PUBLIC_SITE_EMAIL}</Link>, and we&apos;ll try to resolve the problem as soon as we can.</p>
             </div>
 
             <br/>
