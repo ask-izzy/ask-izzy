18fdef5778c073b8830d298076abadf8017d4e13
Callum Gare <cgare@infoxchange.org>
2023-01-06 19:42:20 +1100

    Modify resource proxy to use host for domain match

---
+++ pages/api/external-resource-proxy/[[...path]].js
@@ -0,0 +1,53 @@
+/* @flow */
+// We currently can't do with Next.js rewrites due to this bug
+// https://github.com/vercel/next.js/issues/37028 stopping us
+// proxying the google maps api
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+import { unflattenDomain } from "@/helpers/url.helpers"
+
+const allowedDomains = new Set(process.env.DOMAINS_TO_PROXY)
+
+const proxyMiddleware = createProxyMiddleware({
+    router: req => getTargetBaseUrl(req).origin,
+    changeOrigin: true,
+    onProxyReq: (proxyReq, req, res) => {
+        if (!allowedDomains.has(proxyReq.host)) {
+            return res.status(400).send("Domain not allowed");
+        }
+    },
+});
+
+export default function handler(req: any, res: any): void {
+    if (!process.env.DOMAINS_TO_PROXY) {
+        return res.status(400).send("Route not available");
+    }
+    proxyMiddleware(req, res, (result: any) => {
+        if (result instanceof Error) {
+            throw result;
+        }
+    });
+}
+
+export const config = {
+    api: {
+        externalResolver: true,
+        bodyParser: false,
+    },
+};
+
+function getTargetBaseUrl(req: any) {
+    const targetDomain = unflattenDomain(
+        req.headers.host.replace(
+            new RegExp(`.${process.env.NEXT_PUBLIC_PROXY_DOMAIN_SUFFIX || ""}(?::\\d+)?$`),
+            ""
+        )
+    )
+    const baseUrlString = `https://${targetDomain}`
+    // Construct URL object to confirm url is valid
+    try {
+        return new URL(baseUrlString)
+    } catch (error) {
+        throw Error(`Tried to proxy the domain "${baseUrlString}" which is not a valid url`)
+    }
+}
