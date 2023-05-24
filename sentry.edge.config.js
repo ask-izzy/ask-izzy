cb402ac6747eb136e3f97befca3edd03fda62e06
Callum Gare <cgare@infoxchange.org>
2023-05-25 09:57:44 +1000

    draft4

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -8,7 +8,6 @@ getConfig();
 
 if (process.env.NODE_ENV !== "test") {
     Sentry.init({
-        tracesSampleRate: 1.0,
         dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN || "null",
         integrations: [
             new CaptureConsole({
