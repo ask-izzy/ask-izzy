4e31848ed2788b4dc170942788cc02160231d465
Callum Gare <cgare@infoxchange.org>
2023-05-24 16:58:53 +1000

    draft

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -2,11 +2,24 @@
 
 import * as Sentry from "@sentry/nextjs";
 import getConfig from 'next/config';
+import { CaptureConsole, ExtraErrorData } from "@sentry/integrations";
 
 getConfig();
 
 if (process.env.NODE_ENV !== "test") {
     Sentry.init({
         dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN || "null",
+        integrations: [
+            new CaptureConsole({
+                levels: ["warn", "error"],
+            }),
+            new ExtraErrorData(),
+        ],
+        beforeSend(event) {
+            if (event.message?.startsWith("[HPM] Error occurred while proxying request")) {
+                event.message = "[HPM] Error occurred while proxying request"
+            }
+            return event;
+        },
     });
 }
