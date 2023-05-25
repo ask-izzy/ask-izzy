c1ee6912b578ccf6a1c1a054e5cdef9ea5b51f09
Callum Gare <cgare@infoxchange.org>
2023-05-25 13:17:50 +1000

    draft6

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -2,16 +2,12 @@
 
 import * as Sentry from "@sentry/nextjs";
 import getConfig from 'next/config';
-import { CaptureConsole, ExtraErrorData } from "@sentry/integrations";
 
 getConfig();
 
 if (process.env.NODE_ENV !== "test") {
     Sentry.init({
         dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN || "null",
-        ignoreErrors: [
-            /is neither a category or a content page$/,
-            /^Fast Refresh had to perform a full reload/,
-        ],
+        tracesSampleRate: 1.0,
     });
 }
