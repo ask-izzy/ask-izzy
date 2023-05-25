ba9384625aed6f80c2e59d924414ea76cd7a86a1
Callum Gare <cgare@infoxchange.org>
2023-05-25 10:36:20 +1000

    draft5

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -9,21 +9,9 @@ getConfig();
 if (process.env.NODE_ENV !== "test") {
     Sentry.init({
         dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN || "null",
-        integrations: [
-            new CaptureConsole({
-                levels: ["warn", "error"],
-            }),
-            new ExtraErrorData(),
-        ],
         ignoreErrors: [
             /is neither a category or a content page$/,
             /^Fast Refresh had to perform a full reload/,
         ],
-        beforeSend(event) {
-            if (event.message?.startsWith("[HPM] Error occurred while proxying request")) {
-                event.message = "[HPM] Error occurred while proxying request"
-            }
-            return event;
-        },
     });
 }
