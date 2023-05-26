086147db1b28223184403a2111afd377632e88ff
Callum Gare <cgare@infoxchange.org>
2023-05-26 12:30:10 +1000

    Fix sentry related performance regression

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -3,13 +3,11 @@
 import * as Sentry from "@sentry/nextjs";
 import getConfig from 'next/config';
 
-import sharedConfig from "./sentry.shared.config"
-
 getConfig();
 
 if (process.env.NODE_ENV !== "test") {
     Sentry.init({
-        ...sharedConfig,
         dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN || "null",
+        // Importing the shared config triggers a bug in sentry which adds a 2 sec delay to page loads so we don't
     });
 }
