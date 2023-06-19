1ffb7dc0f6c7cc84612c3d37bea99a819c9dc474
Callum Gare <cgare@infoxchange.org>
2023-06-19 18:17:34 +1000

    draft

--- scripts/ix-hosting-build-to-vol.sh
+++ scripts/ix-hosting-build-to-vol.sh
@@ -1,36 +1,39 @@
 #!/usr/bin/env bash
 
+# This file is responsible for coordinating the build on deploy process which hosting Ask Izzy on Infoxchange infrastructure.
+# It is unlikely to be needed when deploying on other platforms.
+
 # Enable strict mode
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 IFS=$'\n\t'
 
-# AWS deployment builds in the serve step rather than the deploy step
-if [ -n "${ECS_AGENT_URI-}" ]; then
-    env
-    curl "${ECS_CONTAINER_METADATA_URI_V4}/task"
-    TASK_DEFINITION_REVISION=$(node -e "fetch('${ECS_CONTAINER_METADATA_URI_V4}/task').then(res => res.json()).then(res => console.log(res['Revision'] ?? ''))")
-    echo --${TASK_DEFINITION_REVISION}--
-    exit 0;
-fi
+env | awk 'ORS="*SPLIT*"'
 
-DEPLOYMENT_ID=$(echo "$HOSTNAME" | awk -F "-" '{print $(NF-1)}')
-echo ------------ Deployment ID: "$DEPLOYMENT_ID" -----------
+# Deployment process relies on knowing which task definition revision is currently being deployed
+if [ -z "$ECS_CONTAINER_METADATA_URI_V4" ]; then
+    echo "Error: AWS ECS container agent endpoint not found. Aborting deployment" 1>&2
+    exit 1
+fi 
 
-# This file is responsible for coordinating the build on deploy process which hosting Ask Izzy on Infoxchange infrastructure.
-# It is unlikely to be needed when deploying on other platforms.
+curl "${ECS_CONTAINER_METADATA_URI_V4}/task"
+TASK_DEFINITION_REVISION=$(node -e "import('node-fetch').then( fetch => fetch.default('${ECS_CONTAINER_METADATA_URI_V4}/task').then(res => res.json()).then(res => console.log(res['Revision'] ?? '')) )")
+echo --${TASK_DEFINITION_REVISION}--
 
-STORAGE_BUILD_DIR="/storage/$VERSION/$DEPLOYMENT_ID"
-STORAGE_COPY_OF_NEXT_DIR="$STORAGE_BUILD_DIR/copy-of-next-dir"
-APP_NEXT_DIR="/app/.next"
+if [ -z "$TASK_DEFINITION_REVISION" ]; then
+    echo "Error: Failed to fetch task definition revision from AWS ECS container agent endpoint. Aborting deployment" 1>&2
+    exit 1
+fi 
+
+echo ------------ Deploy Stage - Task Definition Revision: "$TASK_DEFINITION_REVISION" -----------
+
+STORAGE_BUILD_DIR="/storage/$TASK_DEFINITION_REVISION"
 BUILD_COMPLETE_FILE="$STORAGE_BUILD_DIR/.build-complete"
+NUM_OF_PREVIOUS_REVISIONS_TO_KEEP=5 # Keep build files for the last 5 task definition revisions
 
 # The first container that attempts to create the build dir is responsible for building.
 # If the build dir creation fails it means another container has already claimed that job
 # so we just wait until it's done.
-
-STORAGE_BUILD_PARENT_DIR="$(dirname "$STORAGE_BUILD_DIR")"
-mkdir -p "$STORAGE_BUILD_PARENT_DIR" # "-p" because we don't want an error if it already exists
 
 CREATE_STORAGE_BUILD_DIR_LOG=$(mkdir "$STORAGE_BUILD_DIR" 2>&1) \
     && CREATE_STORAGE_BUILD_DIR_EXIT_STATUS=$? \
@@ -39,33 +42,19 @@ CREATE_STORAGE_BUILD_DIR_LOG=$(mkdir "$STORAGE_BUILD_DIR" 2>&1) \
 if [ "$CREATE_STORAGE_BUILD_DIR_EXIT_STATUS" -eq 0 ]; then
     echo "This container is in charge of building the app"
     yarn build
-    cp -a "$APP_NEXT_DIR" "$STORAGE_COPY_OF_NEXT_DIR"
+
+    # Once we've finished building copy all files into the /storage subdirectory
+    time cp -a "./." "$STORAGE_BUILD_DIR"
 
     # Create build complete file to let other containers know we've finished building
     touch "$BUILD_COMPLETE_FILE"
 
-    SINGLE_SITE_DOMAIN=$(echo "$SITE_DOMAIN" | awk -F '|' '{ print $1 }')
-    # We actually need word splitting here for the subshell so that the returned values of the subshell are interpreted as separate arguments
-    # shellcheck disable=SC2046
-    CURRENT_DEPLOYED_VERSION=$(curl -sS --fail $([ -n "$HTTP_PROXY" ] && echo -e "-x\n\t$HTTP_PROXY") "$SITE_PROTOCOL://$SINGLE_SITE_DOMAIN/VERSION")
-
-    # ".next" is needed while transitioning to this new build system
-    # and can be removed once all environments have been updated.
-    VERSIONS_TO_KEEP=("/storage/$VERSION" "/storage/$CURRENT_DEPLOYED_VERSION" "/storage/.next")
-
-    for storage_item in /storage/*; do
-        KEEP=false
-        for i in "${VERSIONS_TO_KEEP[@]}"; do
-            if [ "$i" = "$storage_item" ] ; then
-                KEEP=true
-            fi
-        done
-
-        if [ $KEEP != "true" ]; then
-            echo Deleting previous build: "$storage_item"
-            rm -r "$storage_item"
+    for STORAGE_DIR_ITEM in /storage/*; do
+        if [ "$(basename $STORAGE_DIR_ITEM)" -lt $((TASK_DEFINITION_REVISION-NUM_OF_PREVIOUS_REVISIONS_TO_KEEP)) ]; then
+            echo Deleting previous build: "$STORAGE_DIR_ITEM"
+            rm -r "$STORAGE_DIR_ITEM"
         else
-            echo Ignoring: "$storage_item"
+            echo Ignoring: "$STORAGE_DIR_ITEM"
         fi
     done
 else
