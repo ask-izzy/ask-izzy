e1c2c85e2232ebae376115d245dbc0ac3471ab78
ggarcia <ggarcia@infoxchange.org>
2023-06-23 11:30:53 +1000

    Create language spoken field

---
+++ helpers/string.helpers.js
@@ -0,0 +1,18 @@
+/* @flow */
+
+export function shortenText(maxCharacters: number, text: string): string {
+    if (text.length <= maxCharacters) {
+        return text;
+    }
+    let shortened = text.slice(0, maxCharacters);
+
+    if (text[maxCharacters] !== " " && shortened.lastIndexOf(" ") !== -1) {
+        shortened = shortened.slice(0, shortened.lastIndexOf(" "));
+    }
+
+    if (shortened[shortened.length - 1] === ".") {
+        return shortened;
+    } else {
+        return shortened + "...";
+    }
+}
---
+++ src/components/LanguagesAvailable.js
@@ -0,0 +1,41 @@
+/* @flow */
+
+import type {Node as ReactNode} from "React"
+import React from "react"
+
+import Collapser from "./general/Collapser"
+import Spacer from "./Spacer"
+import {shortenText} from "@/helpers/string.helpers"
+import Service from "../iss/Service"
+import Language from "@/src/icons/Language"
+
+type Props = {
+    service: Service,
+}
+
+export default function LanguagesAvailable({service}: Props): ReactNode {
+    const languagesSpoken = `Languages spoken at this service are ${service.languages.join(", ")}`
+    return (
+        <div className="LanguagesAvailable">
+            <Spacer />
+            <div className="languages-spoken-container">
+                <Language />
+                <Collapser
+                    contentPreview={shortenText(60, languagesSpoken)}
+                    expandMessage="Show all languages"
+                    collapseMessage="Hide all languages"
+                    analyticsEvent={{
+                        event: `Action Triggered - Languages Spoken`,
+                        eventAction: "Show full languages spoken",
+                        eventLabel: null,
+                    }}
+                >
+                    {languagesSpoken}
+                </Collapser>
+
+            </div>
+        </div>
+    )
+
+}
+
