19abc1fb9064e768dce4ac3f87fc35193d462f27
Callum Gare <cgare@infoxchange.org>
2023-07-24 13:17:53 +1000

    draft3

--- sentry.shared.config.js
+++ sentry.shared.config.js
@@ -8,9 +8,9 @@ export default function initSentry(additionalConfig: any = {}) {
     if (process.env.NODE_ENV !== "test") {
         Sentry.init({
             dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN || "null",
-            tracesSampleRate: isNaN(process.env.INTERNAL_SENTRY_TRACES_SAMPLE_RATE) 
+            tracesSampleRate: isNaN(process.env.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE) 
                 ? 1.0
-                : parseFloat(process.env.INTERNAL_SENTRY_TRACES_SAMPLE_RATE),
+                : parseFloat(process.env.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE),
             environment: process.env.ENVIRONMENT || process.env.NODE_ENV,
             ignoreErrors: [
                 /is neither a category or a content page$/,
