29f6487942b2b601f9a3edc313ee665ae0910795
Callum Gare <cgare@infoxchange.org>
2023-07-24 15:14:56 +1000

    draft 6

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -5,9 +5,10 @@ import getConfig from 'next/config';
 
 getConfig();
 
+// Importing the shared sentry config init function triggers a bug in sentry which adds a 2 sec delay to page 
+// loads so we don't use it
 if (process.env.NODE_ENV !== "test") {
     Sentry.init({
         dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN || "null",
-        // Importing the shared config triggers a bug in sentry which adds a 2 sec delay to page loads so we don't
     });
 }
