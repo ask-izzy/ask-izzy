f98b1aab72661d09184201cfde76e193cc9ee697
YookesDev <yookesbusiness@gmail.com>
2024-04-16 12:19:53 +1000

    AI-344 adding my front end changes

--- src/components/PhoneButton.js
+++ src/components/PhoneButton.js
@@ -1,17 +1,16 @@
 /* @flow */
-import type {Node as ReactNode} from "React";
+import type { Node as ReactNode } from "React";
 import React from "react";
 import { titleize } from "underscore.string";
 
 import Link from "./base/Link";
-import type {AnalyticsEvent} from "../google-tag-manager";
+import type { AnalyticsEvent } from "../google-tag-manager";
 import icons from "../icons";
-import classnames from "classnames"
+import classnames from "classnames";
 import type { phone } from "../iss/general";
-import {toCamelCase} from "../utils/strings";
 import ScreenReader from "./ScreenReader";
-import FormatText from "./FormatText"
-import {PhoneHref} from "@/helpers/regex.helpers"
+import FormatText from "./FormatText";
+import { PhoneHref } from "@/helpers/regex.helpers";
 
 type Props = phone & {
     crisis?: boolean,
@@ -21,8 +20,8 @@ type Props = phone & {
     analyticsEventDetails?: AnalyticsEvent,
     className?: ?string,
     hasDetails?: ReactNode,
-    styleType?: string // currently only "hollow" is supported
-}
+    styleType?: string, // currently only "hollow" or "link" is supported
+};
 
 function PhoneButton({
     crisis,
@@ -36,10 +35,10 @@ function PhoneButton({
 }: Props): ReactNode {
     const icon = styleType === "hollow" ?
         <icons.Phone />
-        : <icons.PhoneSolid />
+        : <icons.PhoneSolid />;
 
     function displayComment(): string {
-        return comment ? comment : titleize(kind)
+        return comment ? comment : titleize(kind);
     }
 
     return (
@@ -47,47 +46,46 @@ function PhoneButton({
             classnames(
                 "Contact",
                 "Phone",
-                styleType ?
-                    ` ${toCamelCase("style " + styleType)}`
-                    : ""
-            )
-        }
+                "link-style" // adding a specific class for the link style
+            )}
         >
+
             <ScreenReader>
-                    Phone contact:
+                Phone contact:
             </ScreenReader>
             <Link
                 to={PhoneHref(number)}
-                className="ContactButton"
+                className={styleType === "link" ? "ContactLink" : "ContactButton"}
                 analyticsEvent={{
                     event: "Link Followed - Phone Contact",
-                    eventAction: `Contact detail - phone` +
-                            `${crisis ? " - crisis line" : ""}`,
+                    eventAction: `Contact detail - phone${crisis ? " - crisis line" : ""}`,
                     eventLabel: `${number}`,
                     ...analyticsEventDetails,
                 }}
             >
+
                 <div className="Contact-text">
                     {icon}
                     <span>Call </span>
                     <span className="number value">
                         {number}
                     </span>
+                    {styleType === "link" && <span>{" (" + displayComment() + ")"}</span>}
                 </div>
             </Link>
-            <div className="details-container">
-                <FormatText>{displayComment()}</FormatText>
-                {
-                    hasDetails &&
-                    <div className="crisis-line-details">
-                        {hasDetails}
-                        Call Info
-                    </div>
-                }
-            </div>
-
+            {styleType !== "link" && (
+                <div className="details-container">
+                    <FormatText>{displayComment()}</FormatText>
+                    {hasDetails && (
+                        <div className="crisis-line-details">
+                            {hasDetails}
+                            Call Info
+                        </div>
+                    )}
+                </div>
+            )}
         </div>
-    )
+    );
 }
 
-export default PhoneButton
+export default PhoneButton;
