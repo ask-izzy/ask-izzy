1c01c131d21151aa1ba9af89b2d3d5e7f72ba583
YookesDev <yookesbusiness@gmail.com>
2024-04-22 12:50:42 +1000

    AI-344 Fixed changes to the CSS based on feedback and discussion

--- src/components/PhoneButton.js
+++ src/components/PhoneButton.js
@@ -1,16 +1,17 @@
 /* @flow */
-import type { Node as ReactNode } from "React";
+import type {Node as ReactNode} from "React";
 import React from "react";
 import { titleize } from "underscore.string";
 
 import Link from "./base/Link";
-import type { AnalyticsEvent } from "../google-tag-manager";
+import type {AnalyticsEvent} from "../google-tag-manager";
 import icons from "../icons";
-import classnames from "classnames";
+import classnames from "classnames"
 import type { phone } from "../iss/general";
+import {toCamelCase} from "../utils/strings";
 import ScreenReader from "./ScreenReader";
-import FormatText from "./FormatText";
-import { PhoneHref } from "@/helpers/regex.helpers";
+import FormatText from "./FormatText"
+import {PhoneHref} from "@/helpers/regex.helpers"
 
 type Props = phone & {
     crisis?: boolean,
@@ -20,8 +21,8 @@ type Props = phone & {
     analyticsEventDetails?: AnalyticsEvent,
     className?: ?string,
     hasDetails?: ReactNode,
-    styleType?: string, // currently only "hollow" or "link" is supported
-};
+    styleType?: string // currently only "hollow" is supported
+}
 
 function PhoneButton({
     crisis,
@@ -35,10 +36,10 @@ function PhoneButton({
 }: Props): ReactNode {
     const icon = styleType === "hollow" ?
         <icons.Phone />
-        : <icons.PhoneSolid />;
+        : <icons.PhoneSolid />
 
     function displayComment(): string {
-        return comment ? comment : titleize(kind);
+        return comment ? comment : titleize(kind)
     }
 
     return (
@@ -46,12 +47,11 @@ function PhoneButton({
             classnames(
                 "Contact",
                 "Phone",
-                "link-style" // adding a specific class for the link style
+                "link-style"
             )}
         >
-
             <ScreenReader>
-                Phone contact:
+                    Phone contact:
             </ScreenReader>
             <Link
                 to={PhoneHref(number)}
@@ -59,33 +59,31 @@ function PhoneButton({
                 analyticsEvent={{
                     event: "Link Followed - Phone Contact",
                     eventAction: `Contact detail - phone${crisis ? " - crisis line" : ""}`,
+
                     eventLabel: `${number}`,
                     ...analyticsEventDetails,
                 }}
             >
-
                 <div className="Contact-text">
-                    {icon}
-                    <span>Call </span>
+                    {icon}<span>Call </span>
                     <span className="number value">
                         {number}
                     </span>
-                    {styleType === "link" && <span>{"(" + displayComment() + ")"}</span>}
                 </div>
             </Link>
-            {styleType !== "link" && (
-                <div className="details-container">
-                    <FormatText>{displayComment()}</FormatText>
-                    {hasDetails && (
-                        <div className="crisis-line-details">
-                            {hasDetails}
-                            Call Info
-                        </div>
-                    )}
-                </div>
-            )}
+            <div className="details-container">
+                <FormatText>{displayComment()}</FormatText>
+                {
+                    hasDetails &&
+                    <div className="crisis-line-details">
+                        {hasDetails}
+                        Call Info
+                    </div>
+                }
+            </div>
+
         </div>
-    );
+    )
 }
 
-export default PhoneButton;
+export default PhoneButton
