677bae464c24571601489c0bac63b9ce9e05cbca
YookesDev <yookesbusiness@gmail.com>
2024-05-03 11:01:33 +1000

    AI-312-Adding-Cost-Field

--- src/components/CostField.js
+++ src/components/CostField.js
@@ -2,8 +2,6 @@
 
 import React, { useState } from "react";
 import FormatText from "./FormatText";
-import TooltipInformation from "@/src/components/TooltipInformation";
-import Info from "../icons/Info";
 import Collapser from "@/src/components/general/Collapser";
 
 type Props = {
@@ -14,7 +12,6 @@ type Props = {
 function Cost({ catchment, cost }: Props) {
     const [showAll, setShowAll] = useState(false);
     const isExceedingLimit = cost && cost.length > 100;
-    const isToolTip = cost.toLowerCase().includes("inquire");
 
     const toggleShowAll = () => {
         setShowAll(!showAll);
@@ -42,32 +39,12 @@ function Cost({ catchment, cost }: Props) {
         );
     }
 
-    if (isToolTip) {
-        costContent = (
-            <>
-                <div className="tooltip-wrapper">
-                    <TooltipInformation
-                        content="You may have to pay a fee to use this service. Contact them to find out more."
-                    >
-                        <div
-                            className="info-icon-container"
-                            aria-label="More information on cost"
-                        >
-                            <Info />
-                        </div>
-                    </TooltipInformation>
-                    <h3 className="tooltip-caption">More info</h3>
-                </div>
-            </>
-        );
-    }
+
 
     return (
         <div className="Cost">
-            <div className={isToolTip ? "tooltip-caption" : "Cost-desc"}>
-                <h2 aria-label="Cost">Cost</h2>
-                <br /> <ul> <li> {costContent} </li> </ul>
-            </div>
+            <h2 aria-label="Cost">Cost</h2>
+            <br /> <ul> <li> {costContent} </li> </ul>
         </div>
     );
 }
