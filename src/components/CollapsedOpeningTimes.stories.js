c8b372c20e4766e37b1e17d4b0729bbdc5e8f35f
YookesDev <yookesbusiness@gmail.com>
2024-05-28 09:28:33 +1000

    AI-343 Added the new OpeningTimesList Component whilst adjusting files related to this component

--- src/components/CollapsedOpeningTimes.stories.js
+++ src/components/CollapsedOpeningTimes.stories.js
@@ -3,97 +3,35 @@
 import type {Node as ReactNode} from "React";
 import React from "react";
 
-import CollapsedOpeningTimes from "./CollapsedOpeningTimes";
-import { mockedTimeDecorator, addTimesToStory } from "./OpeningTimes.stories";
+import Collapser from "./general/Collapser";
+import CurrentOpeningTime from "./CurrentOpeningTime";
+import OpeningTimesList from "./OpeningTimesList";
+import Service from "../iss/Service";
 
-export default {
-    title: "Service Components/OpeningTimes/CollapsedOpeningTimes",
-    component: CollapsedOpeningTimes,
-    decorators: [
-        mockedTimeDecorator,
-    ],
-};
+type Props = {
+    service : Service
+}
 
-const Template = (args: Object): ReactNode => {
-    (Template.args: any); return <CollapsedOpeningTimes {...args} />;
-};
+function CollapsedOpeningTimes({ service }: Props): ReactNode {
+    return (
+        <div className="CollapsedOpeningTimes">
+            <CurrentOpeningTime serviceOpening={service.open} />
+            {service.open.openingTimes.length > 0 && (
+                <Collapser
+                    expandMessage="Show open hours"
+                    analyticsEvent={{
+                        event: `Action Triggered - Opening Times`,
+                        eventAction: "Show opening times",
+                        eventLabel: null,
+                    }}
+                >
+                    <div className="AllOpeningTimes">
+                        <OpeningTimesList service={service}/>
+                    </div>
+                </Collapser>
+            )}
+        </div>
+    )
+}
 
-export const CurrentlyOpen: typeof Template = Template.bind({});
-addTimesToStory(CurrentlyOpen, [
-    {
-        day: "Wednesday",
-        open: "09:00:00",
-        close: "15:00:00",
-    },
-]);
-
-export const BetweenTwiceInOneDay: typeof Template = Template.bind({});
-addTimesToStory(BetweenTwiceInOneDay, [
-    {
-        day: "Wednesday",
-        open: "09:00:00",
-        close: "11:00:00",
-    },
-    {
-        day: "Wednesday",
-        open: "16:00:00",
-        close: "18:00:00",
-    },
-]);
-
-export const BeforeTwiceInOneDay: typeof Template = Template.bind({});
-addTimesToStory(BeforeTwiceInOneDay, [
-    {
-        day: "Wednesday",
-        open: "13:30:00",
-        close: "14:00:00",
-    },
-    {
-        day: "Wednesday",
-        open: "16:00:00",
-        close: "18:00:00",
-    },
-]);
-
-export const AfterTwiceInOneDay: typeof Template = Template.bind({});
-addTimesToStory(AfterTwiceInOneDay, [
-    {
-        day: "Wednesday",
-        open: "01:00:00",
-        close: "02:00:00",
-    },
-    {
-        day: "Wednesday",
-        open: "03:00:00",
-        close: "04:00:00",
-    },
-]);
-
-export const WithManyTimes: typeof Template = Template.bind({});
-addTimesToStory(WithManyTimes, [
-    {
-        day: "Wednesday",
-        open: "01:00:00",
-        close: "02:00:00",
-    },
-    {
-        day: "Thursday",
-        open: "03:00:00",
-        close: "04:00:00",
-    },
-    {
-        day: "Friday",
-        open: "03:00:00",
-        close: "04:00:00",
-    },
-    {
-        day: "Monday",
-        open: "03:00:00",
-        close: "04:00:00",
-    },
-    {
-        day: "Tuesday",
-        open: "03:00:00",
-        close: "04:00:00",
-    },
-]);
+export default CollapsedOpeningTimes;
