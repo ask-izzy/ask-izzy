7cf7d618da19eadc3ee7ce6b6f12e2a34d0598a5
Rahma <rhm758835@gmail.com>
2024-06-19 10:36:10 +1000

    Lint fixes and remove duplicate scss class

--- src/components/CollapsedOpeningTimes.js
+++ src/components/CollapsedOpeningTimes.js
@@ -2,15 +2,12 @@
 import type {Node as ReactNode} from "React";
 import moment from "moment-timezone";
 import React from "react";
+import ServiceOpening from "../../src/iss/ServiceOpening";
 import Collapser from "./general/Collapser";
 import CurrentOpeningTime from "./CurrentOpeningTime.js";
 import OpeningTimesList from "./OpeningTimesList";
 import Service from "../iss/Service";
 import type {openingHours as openingHoursType} from "../iss/general";
-
-function formatTime(str: string): string {
-    return moment(str, "HH:mm:ss").format("h:mm A");
-}
 
 const sampleTime = moment("2015-09-09 1pm", "YYYY-MM-DD ha");
 
